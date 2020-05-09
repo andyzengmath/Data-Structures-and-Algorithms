e254f03031891171fe44d0d4ce1baba026a67e9a
Andy Zeng <acjzqy@gmail.com>
2020-05-08 22:08:24 -0400

    english ruler

--- Data Structures and Algorithms in Python/4. Recursion/recursion.py
+++ Data Structures and Algorithms in Python/4. Recursion/recursion.py
@@ -18,17 +18,17 @@ def draw_line(tick_length, tick_label = ''):
     
 def draw_interval(center_length):
     #draw tick interval based upon a central tick length
-    if center_length > 0:
-        draw_interval(center_length - 1)
-        draw_line(center_length)
-        draw_interval(center_length - 1)
+    if center_length > 0:                 #stops when lengths drops to 0
+        draw_interval(center_length - 1)  #recursively draw top ticks
+        draw_line(center_length)          #draw the center tick
+        draw_interval(center_length - 1)  #recursively draw bottom ticks
             
 def draw_ruler(num_inches, major_length):
     # draw English ruler with given number of inches, major tick length
-    draw_line(major_length, '0')
+    draw_line(major_length, '0')          # draw inch 0 line
     for j in range(1, 1 + num_inches):
-        draw_interval(major_length - 1)
-        draw_line(major_length, str(j))
+        draw_interval(major_length - 1)   #draw interior ticks for inch
+        draw_line(major_length, str(j))   # draw inch j line and label
             
 #draw_ruler(1, 3)
 
