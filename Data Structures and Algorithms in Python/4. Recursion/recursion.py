6fe41e9309bf6bfee382835e299305e0def15cce
Andy Zeng <acjzqy@gmail.com>
2020-05-08 21:40:39 -0400

    update English ruler

--- Data Structures and Algorithms in Python/4. Recursion/recursion.py
+++ Data Structures and Algorithms in Python/4. Recursion/recursion.py
@@ -1,4 +1,4 @@
-# 4.1.1 factorial
+# 4.1.1 factorial function
 def factorial(n):
     if n == 0:
         return 1
@@ -10,24 +10,25 @@ def factorial(n):
 
 def draw_line(tick_length, tick_label = ''):
     #draw one line with given tick length ( folowed by optional label)
+    
     line = '-' * tick_length
     if tick_label:
         line += ' ' + tick_label
     print(line)
     
 def draw_interval(center_length):
-        #draw tick interval based upon a central tick length
-        if center_length > 0:
-            draw_interval(center_length - 1)
-            draw_line(center_length)
-            draw_interval(center_length - 1)
+    #draw tick interval based upon a central tick length
+    if center_length > 0:
+        draw_interval(center_length - 1)
+        draw_line(center_length)
+        draw_interval(center_length - 1)
             
 def draw_ruler(num_inches, major_length):
-        # draw English ruler with given number of inches, major tick length
-        draw_line(major_length, '0')
-        for j in range(1, 1 + num_inches):
-            draw_interval(major_length - 1)
-            draw_line(major_length, str(j))
+    # draw English ruler with given number of inches, major tick length
+    draw_line(major_length, '0')
+    for j in range(1, 1 + num_inches):
+        draw_interval(major_length - 1)
+        draw_line(major_length, str(j))
             
 #draw_ruler(1, 3)
 
