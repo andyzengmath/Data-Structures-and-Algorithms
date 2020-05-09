6c4fec60154b22c61ebcb4859bf07f38f82982c0
Andy Zeng <acjzqy@gmail.com>
2020-05-08 23:34:09 -0400

    add tail recursion

--- Data Structures and Algorithms in Python/4. Recursion/recursion.py
+++ Data Structures and Algorithms in Python/4. Recursion/recursion.py
@@ -141,3 +141,23 @@ def binary_sum(S, start, stop):
         mid = (start + stop)//2
         return binary_sum(S, start, mid) + binary_sum(S, mid, stop)
 # O(n) 
+
+# 4.6 eliminate tail recursion
+def binary_search_iterative(data, target):
+    low = 0
+    high = len(data) -1
+    while low <= high:
+        mid = (low + high)//2
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return False
+
+def reverse_iterative(S):
+    start, stop = 0, len(S)
+    while start < stop - 1:
+        S[start], S[stop -1] = S[stop - 1], S[start]
+        start, stop = start + 1, stop - 1
