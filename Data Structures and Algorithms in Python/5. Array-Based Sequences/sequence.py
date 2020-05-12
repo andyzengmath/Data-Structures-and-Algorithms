b40eb6d7832f489b7a631bf165643bad1b0f6486
Andy Zeng <acjzqy@gmail.com>
2020-05-12 17:57:20 -0400

    add dynamic array

--- Data Structures and Algorithms in Python/5. Array-Based Sequences/sequence.py
+++ Data Structures and Algorithms in Python/5. Array-Based Sequences/sequence.py
@@ -1,9 +1,70 @@
 # 5-1 length of list
 import sys 
-n = 100
+
 data = []
-for k in range(n):
-    a = len(data)
-    b = sys.getsizeof(data)
-    print('Length: {0: 3d}; Size in bytes: {1: 4d}'.format(a, b))
-    data.append(None)
+def list_length(n):
+    for k in range(n):
+        a = len(data)
+        b = sys.getsizeof(data)
+        print('Length: {0: 3d}; Size in bytes: {1: 4d}'.format(a, b))
+        data.append(None)
+#list_length(100)
+
+# 5-3 Dynamic array class
+import ctypes
+
+class DynamicArray:
+    # A dynamic array class to a simplified Python list
+    
+    def __init__(self):
+        self._n = 0 # count actual elements
+        self._capacity = 1 # default array capacity
+        self._A = self._make_array(self._capacity)
+        
+    def __len__(self):
+        return self._n
+    
+    def __getitem__(self, k):
+        if not 0 <= k < self._n:
+            raise IndexError('invalid index')
+        return self._A[k]
+    
+    def append(self, obj):
+        # add object to the end of the array
+        if self._n == self._capacity:
+            self._resize(2 * self._capacity)
+        self._A[self._n] = obj
+        self._n += 1
+        
+    def _resize(self, c):
+        # Resize internal array to capacity c
+        B = self._make_array(c)
+        for k in range(self._n):
+            B[k] = self._A[k]
+        self._A = B
+        self._capacity = c
+        
+    def _make_array(self, c):
+        return (c * ctypes.py_object)()
+    
+    def insert(self, k, value):
+        # insert value at index k, shifting subsequent values rightward
+        if self._n == self._capacity:
+            self._resize(2 * self._capacity)
+        for j in range(self._n, k, -1):
+            self._A[j] = self._A[j-1]
+        self._A[k] = value
+        self._n += 1
+    
+    
+# Amortization analysis
+from time import time
+def compute_average(n):
+    data = []
+    start = time()
+    for k in range(n):
+        data.append(None)
+    end = time()
+    return (end - start) / n
+
+#print(compute_average(100))
