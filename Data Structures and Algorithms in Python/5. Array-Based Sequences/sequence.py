7ea1d43dc9153a3075c5ed71535f3a594203e0a3
Andy Zeng <acjzqy@gmail.com>
2020-05-15 01:50:36 -0400

    add Scoreboard

--- Data Structures and Algorithms in Python/5. Array-Based Sequences/sequence.py
+++ Data Structures and Algorithms in Python/5. Array-Based Sequences/sequence.py
@@ -90,3 +90,57 @@ for k in range(1, n + 1):
 # list comprehension
 squares = [k*k for k in range(1, n + 1)]
 
+# 5.4.2 Strings
+
+# letters = ''.join([c for c in document if c.isalpha()])
+# or letters = ''.join(c for c in document if c.isalpha())
+
+# 5.5.1 Game score
+
+class GameEntry:
+    # Represents one entry of a list of high scores
+    
+    def __init__(self, name, score):
+        self._name = name
+        self._score = score
+        
+    def get_name(self):
+        return self._name
+    
+    def get_score(self):
+        return self._score
+    
+    def __str__(self):
+        return '({0}, {1})'.format(self._name, self._score) # e.g. '(Bob, 98)
+    
+class Scoreboard:
+    # Fixed length sequence of high scores in nondecreasing order
+    
+    def __init__(self, capacity = 10):
+        # Initialize scoreboard with given maximum capacity
+        # All entries are initially None
+        
+        self._board = [None] * capacity
+        self._n = 0
+        
+    def __getitem__(self, k):
+        return self._board[k]
+    
+    def __str__(self, k):
+        # Return string representation of the high score list
+        return '\n'.join(str(self._board[j]) for j in range(self._n))
+    
+    def add(self, entry):
+        score = entry.get_score()
+        # Does this entry qualified as a high score : either board is not full or the score is higher than last entry
+        good = self._n < len(self._board) or score > self._board[-1].get_score()
+        
+        if good:
+            if self._n < len(self._board):
+                self._n +=1
+        j = self._n - 1
+        while j > 0 and self._board[j-1].get_score() < score:
+            self._board[j] = self._board[j - 1]
+            j -= 1
+        self._board[j] = entry
+
