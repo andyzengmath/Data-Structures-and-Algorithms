3df7961a54ec4ff514fd4d415426515e5de4a1cf
Andy Zeng <acjzqy@gmail.com>
2020-05-15 22:25:36 -0400

    add match([{

--- Data Structures and Algorithms in Python/6. Stack queue/stack-queue.py
+++ Data Structures and Algorithms in Python/6. Stack queue/stack-queue.py
@@ -45,3 +45,20 @@ def reverse_file(filename):
     while not S.is_empty():
         output.write(S.pop() + '\n')
     output.close()
+    
+# 6.1.4 match
+
+def is_matched(expr):
+    # Return true if all delimiters are properly match; False otherwise
+    lefty = '({['
+    righty = ')}]'
+    S = ArrayStack()
+    for c in expr:
+        if c in lefty:
+            S.push(c)
+        elif c in righty:
+            if S.is_empty():
+                return False
+            if righty.index(c) != lefty.index(S.pop()):
+                return False
+    return S.is_empty()
