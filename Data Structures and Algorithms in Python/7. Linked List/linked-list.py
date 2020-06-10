b472f51b4f789f46b002bdc1056249263010cb92
Andy Zeng <acjzqy@gmail.com>
2020-06-10 14:04:54 -0400

    add linked Stack

---
+++ Data Structures and Algorithms in Python/7. Linked List/linked-list.py
@@ -0,0 +1,42 @@
+
+        
+class LinkedStack:
+    # LIFO Stack implementation using a singly lined list for storage
+    
+    class _Node:
+        # Lightweight, non public class for storing a singly linked node
+        __slots__ = "_element", '_next'  # streamline memory usage
+        
+        def __init__(self, element, next):
+            self._element = element
+            self._next = next 
+    
+    def __init__(self):
+        self._head = None
+        self._size = 0
+        
+    def __len__(self):
+        return self._size
+    
+    def is_empty(self):
+        return self._size == 0
+    
+    def push(self, e):
+        # add element to the top of the stack
+        self._head = self._Node(e, self._head)
+        self._size +=1
+        
+    def top(self):
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._head._element
+    
+    def pop(self):
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        answer = self._head._element
+        self._head = self._head._next
+        self._size -= 1
+        return answer
+        
+        
