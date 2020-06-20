45ebb54920ded3207d4e8353be46b44dceba4412
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-06-20 18:28:39 -0400

    add A+B

---
+++ Lintcode/1. A + B Problem/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+     /**
+     * @param a: An integer
+     * @param b: An integer
+     * @return: The sum of a and b 
+     */
+    public int aplusb(int a, int b) {
+        // add two integer without using plus operator
+        while (b != 0){
+            int _a = a ^ b;
+            int _b = (a & b) << 1;
+            a = _a;
+            b = _b;
+        }
+        return a;
+}
