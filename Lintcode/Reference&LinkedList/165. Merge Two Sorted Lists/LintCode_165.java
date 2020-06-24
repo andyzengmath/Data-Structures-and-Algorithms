b79dd7ec49c8c38a6a04c3513774e46b744b8586
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-06-24 09:41:36 -0400

    Update LintCode_165.java
    Total runtime 425 ms beats 70.80% Submissions!

--- Lintcode/Reference&LinkedList/165. Merge Two Sorted Lists/LintCode_165.java
+++ Lintcode/Reference&LinkedList/165. Merge Two Sorted Lists/LintCode_165.java
@@ -17,7 +17,48 @@ public class LintCode_165 {
      * @return: ListNode head of linked list
      */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode cur = null;
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+        ListNode cur = null; 
+
+        if (l1.val < l2.val) {
+            cur = l1;
+            cur1 = l1.next;
+            
+        } else {         
+            cur = l2;
+            cur2 = l2.next;
+        }
+
+        ListNode head = cur;
+
+        while (cur1 != null && cur2 != null) {
+            
+            if (cur1.val < cur2.val) {
+                cur.next = cur1;
+                cur = cur1;
+                cur1 = cur1.next;
+                
+            } else if (cur2.val <= cur1.val) {
+                cur.next = cur2;
+                cur = cur2;
+                cur2 = cur2.next;
+                
+            }
+        }
         
+        if (cur1 != null) {
+            cur.next = cur1;
+        }
+        
+        if (cur2 != null) {
+            cur.next = cur2;
+        } 
+        return head;
     }
 }
