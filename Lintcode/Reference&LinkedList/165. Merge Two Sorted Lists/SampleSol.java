3b1d6093a06bd7cf48d0261a5f0b89face94a480
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-06-24 09:47:32 -0400

    Create SampleSol.java

---
+++ Lintcode/Reference&LinkedList/165. Merge Two Sorted Lists/SampleSol.java
@@ -0,0 +1,25 @@
+public class SampleSol {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode temp = new ListNode(0);
+        ListNode lastNode = temp;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                lastNode.next = l1;
+                l1 = l1.next;
+            } else {
+                lastNode.next = l2;
+                l2 = l2.next;
+            }
+            lastNode = lastNode.next;
+        }
+
+        if (l1 != null) {
+            lastNode.next = l1;
+        } else {
+            lastNode.next = l2;
+        }
+
+        return temp.next;
+    }
+}
