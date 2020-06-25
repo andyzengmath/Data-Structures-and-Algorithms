b0948dc5451de94366772e3edf9acc5820e157c9
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-06-24 23:19:23 -0400

    add Maximum Number of Prizes problem

--- Algorithm-Toolbox-Coursera/week3_greedy_algorithms/6_maximum_number_of_prizes/DifferentSummands.java
+++ Algorithm-Toolbox-Coursera/week3_greedy_algorithms/6_maximum_number_of_prizes/DifferentSummands.java
@@ -4,7 +4,22 @@ public class DifferentSummands {
     private static List<Integer> optimalSummands(int n) {
         List<Integer> summands = new ArrayList<Integer>();
         //write your code here
-        return summands;
+        int rem = n;
+        int currPrize = 1;
+
+        while (rem > 0 && currPrize <= rem) {
+            summands.add(currPrize);
+            rem = rem - currPrize;
+            //System.out.print(currPrize);
+            //System.out.println("curr");
+            currPrize += 1; 
+        }
+        if (rem == 0) {
+            return summands;
+        } else {
+            summands.set(currPrize - 2, currPrize + rem - 1);
+            return summands;
+        }
     }
     
     public static void main(String[] args) {
---
+++ Lintcode/82. Single Number/LintCode_82.java
@@ -0,0 +1,10 @@
+public class LintCode_82 {
+    /**
+     * @param A: An integer array
+     * @return: An integer
+     * c.f. LeetCode 136
+     */
+    public int singleNumber(int[] A) {
+        // write your code here
+    }
+}
