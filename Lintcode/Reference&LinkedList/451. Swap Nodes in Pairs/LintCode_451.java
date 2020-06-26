5085fe06a9cec7ecddbdeb1be6ae05bd9e7a9c62
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-06-26 17:16:42 -0400

    Create LintCode_451.java
    Total runtime 230 ms, beats 94.60% Submissions!

---
+++ Lintcode/Reference&LinkedList/451. Swap Nodes in Pairs/LintCode_451.java
@@ -0,0 +1,30 @@
+public class LintCode_451 {
+    /**
+     * @param head: a ListNode
+     * @return: a ListNode
+     */
+    public ListNode swapPairs(ListNode head) {
+        // write your code here
+        ListNode curr = head;
+        ListNode temp = null;
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+
+        while (curr != null) {
+            if (curr.next == null) {
+                break;
+            } else {
+                temp = curr.next.next;
+                pre.next = curr.next;
+                pre.next.next = curr;
+                curr.next = temp;
+    
+                pre = curr;
+                curr = curr.next;
+            }
+        }
+
+        return dummy.next;
+}
