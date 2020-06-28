22211657fc8244764d99d1c724a3a6f64536350e
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-06-28 00:57:57 -0400

    Update Inversions.java

--- Algorithm-Toolbox-Coursera/week4_divide_and_conquer/4_number_of_inversions/Inversions.java
+++ Algorithm-Toolbox-Coursera/week4_divide_and_conquer/4_number_of_inversions/Inversions.java
@@ -4,15 +4,50 @@ public class Inversions {
 
     private static long getNumberOfInversions(int[] a, int[] b, int left, int right) {
         long numberOfInversions = 0;
-        if (right <= left + 1) {
+        if (right <= left) {
             return numberOfInversions;
         }
-        int ave = (left + right) / 2;
+        int ave = left + (right - left) / 2;
+
         numberOfInversions += getNumberOfInversions(a, b, left, ave);
-        numberOfInversions += getNumberOfInversions(a, b, ave, right);
-        //write your code here
+        numberOfInversions += getNumberOfInversions(a, b, ave + 1, right);
+        numberOfInversions += merge(a, b, left, ave + 1, right);
         return numberOfInversions;
     }
+
+    private static long merge(int[] a, int[] b,  int left, int ave, int right) {
+        int i = left, j = ave, k = left;
+        long inversions = 0;
+
+        while (i <= ave - 1 && j <= right) {
+            if (a[i] <= a[j]) {
+                b[k] = a[i];
+                i ++;
+            } else {
+                b[k] = a[j];
+                inversions += ave - i;
+                j++;
+            }
+            k++;       
+        }
+
+        while (i <= ave - 1) {
+            b[k] = a[i];
+            i++;
+            k++;
+        }
+        while (j <= right) {
+            b[k] = a[j];
+            j++;
+            k++;
+        }
+
+        for (i = left; i <= right; i++) {
+            a[i] = b[i];
+        }
+        return inversions;
+    }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -22,7 +57,7 @@ public class Inversions {
             a[i] = scanner.nextInt();
         }
         int[] b = new int[n];
-        System.out.println(getNumberOfInversions(a, b, 0, a.length));
+        System.out.println(getNumberOfInversions(a, b, 0, a.length - 1));
     }
 }
 
