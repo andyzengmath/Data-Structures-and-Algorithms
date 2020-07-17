da177571ae0324662a4ea296f783d0af186d72df
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-07-17 01:33:54 -0400

    Create LintCode_511.java

---
+++ Lintcode/Reference&LinkedList/511. Swap Two Nodes in Linked List/LintCode_511.java
@@ -0,0 +1,11 @@
+public class LintCode_511 {
+    /**
+     * @param head: a ListNode
+     * @param v1: An integer
+     * @param v2: An integer
+     * @return: a new head of singly-linked list
+     */
+    public ListNode swapNodes(ListNode head, int v1, int v2) {
+        // write your code here
+    }
+}
