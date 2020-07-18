f37ad4290856d4bebd4948a1c09db6858da9f614
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-07-17 20:11:02 -0400

    Update LintCode_423.java
    char stack
    Your submission beats 59.20% Submissions!
    Total runtime 181 ms

--- Lintcode/Stack&Queue/423. Valid Parentheses/LintCode_423.java
+++ Lintcode/Stack&Queue/423. Valid Parentheses/LintCode_423.java
@@ -1,3 +1,27 @@
 public class LintCode_423 {
-    
+    /**
+     * @param s: A string
+     * @return: whether the string is a valid parentheses
+     */
+    public boolean isValidParentheses(String s) {
+        // write your code here
+        Stack<Character> stack = new Stack<Character>();
+        for (Character c: s.toCharArray()) {
+            if ("({[".contains(String.valueOf(c))) {
+                stack.push(c);
+            } else {
+                if (!stack.isEmpty() && is_valid(stack.peek(), c)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    private boolean is_valid(char c1, char c2) {
+        return (c1 == '(' && c2 == ')') || (c1 == '[' && c2 == ']') || (c1 == '{' && c2 == '}');
+    }
 }
