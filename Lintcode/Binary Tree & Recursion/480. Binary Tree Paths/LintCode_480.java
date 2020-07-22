b542cff65fc6aa8ebea555d30680576d1252cea7
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-07-22 19:56:16 -0400

    Update LintCode_480.java

--- Lintcode/Binary Tree & Recursion/480. Binary Tree Paths/LintCode_480.java
+++ Lintcode/Binary Tree & Recursion/480. Binary Tree Paths/LintCode_480.java
@@ -7,5 +7,37 @@ public class LintCode_480 {
      */
     public List<String> binaryTreePaths(TreeNode root) {
         // write your code here
+        
+        List<String> result = new ArrayList<String>();
+
+        if (root == null) {
+            return result;
+        }
+
+        dfs(root, String.valueOf(root.val), result);
+        return result;
+
+
+    }
+
+    private void dfs(TreeNode root, String path, List<String> result) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.left == null && root.right == null) {
+            result.add(path);
+            return;
+        }
+
+        if (root.left != null) {
+            dfs(root.left, path + "->" + String.valueOf(root.left.val), result);
+        }
+
+        if (root.right != null) {
+            dfs(root.right, path + "->" + String.valueOf(root.right.val), result);
+        }
+
+
     }
 }
