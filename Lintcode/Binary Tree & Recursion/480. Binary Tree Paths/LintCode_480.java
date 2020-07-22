ca537b7a99c3e888c87e5f96e9bc9fd89cc50f56
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-07-22 19:26:30 -0400

    add 480

--- Lintcode/Binary Tree & Recursion/480. Binary Tree Paths/LintCode_480.java
+++ Lintcode/Binary Tree & Recursion/480. Binary Tree Paths/LintCode_480.java
@@ -1,5 +1,11 @@
 package 480. Binary Tree Paths;
 
 public class LintCode_480 {
-    
+    /**
+     * @param root: the root of the binary tree
+     * @return: all root-to-leaf paths
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+        // write your code here
+    }
 }
