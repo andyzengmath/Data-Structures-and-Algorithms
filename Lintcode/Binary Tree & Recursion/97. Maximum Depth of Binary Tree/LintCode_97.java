7f49f9922b9139fe1405b8aaf7ead98c67326548
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-07-23 00:33:06 -0400

    Create LintCode_97.java

---
+++ Lintcode/Binary Tree & Recursion/97. Maximum Depth of Binary Tree/LintCode_97.java
@@ -0,0 +1,14 @@
+public class LintCode_97 {
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer
+     */
+    public int maxDepth(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
