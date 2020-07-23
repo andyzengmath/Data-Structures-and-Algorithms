e748f44192cd9c39d318458b28b21f6c80c8d431
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-07-22 22:58:04 -0400

    Create LintCode_175.java

---
+++ Lintcode/Binary Tree & Recursion/175. Invert Binary Tree/LintCode_175.java
@@ -0,0 +1,9 @@
+public class LintCode_175 {
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    public void invertBinaryTree(TreeNode root) {
+        // write your code here
+    }
+}
