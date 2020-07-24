17491da20cc8b6ec01c6c24aaae5c246ae2a8078
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-07-24 02:00:14 -0400

    Update PlacingParentheses.java

--- Algorithm-Toolbox-Coursera/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/PlacingParentheses.java
+++ Algorithm-Toolbox-Coursera/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/PlacingParentheses.java
@@ -3,7 +3,38 @@ import java.util.Scanner;
 public class PlacingParentheses {
     private static long getMaximValue(String exp) {
       //write your code here
-      return 0;
+      int n = exp.length / 2 + 1;
+      int [][] min = new int[n][n], max = new int[n][n];
+
+      for (int i = 0; i < n; i ++) {
+          min[i][i] = exp.charAt(i * 2) - '0';
+          max[i][i] = exp.charAt(i * 2) - '0';
+      }
+
+      for (int s = 1; s <= n; s++) {
+          for (int i = 0; i <= n - s - 1; i++) {
+              int j = s + i;
+              int[] result = getMinAndMax(exp, i, j, min, max);
+              min[i][j] = result[0];
+              max[i][j] = result[1];
+          }
+      }
+      return max[0][n-1];
+    }
+
+    public static int[] minAndMax (String exp, int i, int j, int[][] min, int[][] max) {
+        int[] result = new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE};
+
+        for (int index = i; index <= j -1; index ++) {
+            char operator = exp.charAt(index * 2 + 1);
+            long a = eval(min[i][index], max[index + 1][j], operator);
+            long b = eval(min[i][index], min[index + 1][j], operator);
+            long c = eval(max[i][index], max[index + 1][j], operator);
+            long d = eval(max[i][index], min[index + 1][j], operator);
+
+        }
+        result[0] = (int) Math.min(a, Math.min(b, Math.min(c, d)));
+        result[1] = (int) Math.max(a, Math.max(b, Math.max(c, d)));
     }
 
     private static long eval(long a, long b, char op) {
