f09ac96c1e0d1d5ad6915d2374600d334ce968d1
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-07-23 21:51:31 -0400

    Update LintCode_644.java

--- Lintcode/Binary search tree&Hash table/644. Strobogrammatic Number/LintCode_644.java
+++ Lintcode/Binary search tree&Hash table/644. Strobogrammatic Number/LintCode_644.java
@@ -1,3 +1,39 @@
 public class LintCode_644 {
-    
+    /**
+     * @param num: a string
+     * @return: true if a number is strobogrammatic or false
+     */
+    public boolean isStrobogrammatic(String num) {
+        if (num == null || num.length() == 0) {
+            return false;
+        }
+
+        int left = 0;
+        int right = num.length() - 1;
+        while (left <= right) {
+            char leftChar = num.charAt(left);
+            char rightChar = num.charAt(right);
+
+            if (left == right) {
+                if (leftChar == '0' || leftChar == '1'|| leftChar == '8') {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                if (leftChar == rightChar) {
+                    if (leftChar == '0' || leftChar == '1' || leftChar == '8') {
+                        left++;
+                        right--;
+                    } 
+                } else if ((leftChar == '6' && rightChar == '9')||(leftChar == '9' && rightChar == '6')) {
+                    left++;
+                    right--;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
