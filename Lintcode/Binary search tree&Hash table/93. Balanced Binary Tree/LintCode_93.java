b600c254f4713939675df73afad5889adca8b68a
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-07-25 03:01:47 -0400

    Update LintCode_93.java

--- Lintcode/Binary search tree&Hash table/93. Balanced Binary Tree/LintCode_93.java
+++ Lintcode/Binary search tree&Hash table/93. Balanced Binary Tree/LintCode_93.java
@@ -1,3 +1,24 @@
 public class LintCode_93 {
-    
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+    public boolean isBalanced(TreeNode root) {
+        return getDepth(root) != -1;
+
+
+    }
+
+    public int getDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = getDepth(root.left);
+        int right = getDepth(root.right);
+        if (left == rightJ) {
+            return left + 1;
+        } else {
+            return -1;
+        }
+    }
 }
