1c5a22ceb006b9d6f61db74f5ad09121d5e3a386
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-07-25 22:18:53 -0400

    Create LintCode_56.java

---
+++ Lintcode/Binary search tree&Hash table/56. Two Sum/LintCode_56.java
@@ -0,0 +1,12 @@
+public class LintCode_56 {
+    /**
+     * @param numbers: An array of Integer
+     * @param target: target = numbers[index1] + numbers[index2]
+     * @return: [index1 + 1, index2 + 1] (index1 < index2)
+     * Challenge: O(n) Space, O(nlogn) Time
+     * or O(n) Space, O(n) Time
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        // write your code here
+    }
+}
