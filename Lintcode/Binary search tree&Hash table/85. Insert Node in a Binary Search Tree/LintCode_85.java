221f8fb41e63fade64cba34f2f17c809d706962b
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-07-25 21:07:38 -0400

    Create LintCode_85.java

---
+++ Lintcode/Binary search tree&Hash table/85. Insert Node in a Binary Search Tree/LintCode_85.java
@@ -0,0 +1,21 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class LintCode_85 {
+    /*
+     * @param root: The root of the binary search tree.
+     * @param node: insert this node into the binary search tree
+     * @return: The root of the new binary search tree.
+     */
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        // write your code here
+    }
+}
