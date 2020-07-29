c6232c0cfbefe2adfda3160e689ee3350cb348d2
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-07-28 23:36:24 -0400

    Create LintCode_95.java

---
+++ Lintcode/Binary search tree&Hash table/95. Validate Binary Search Tree/LintCode_95.java
@@ -0,0 +1,9 @@
+public class LintCode_95 {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
+    public boolean isValidBST(TreeNode root) {
+        // write your code here
+    }
+}
