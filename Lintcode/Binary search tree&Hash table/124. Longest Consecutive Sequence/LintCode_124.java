b4dcc5755b7515aa8f6c2e17c9270149b4fa7e5d
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-01 12:12:54 -0400

    Create LintCode_124.java

---
+++ Lintcode/Binary search tree&Hash table/124. Longest Consecutive Sequence/LintCode_124.java
@@ -0,0 +1,9 @@
+public class LintCode_124 {
+    /**
+     * @param num: A list of integers
+     * @return: An integer
+     */
+    public int longestConsecutive(int[] num) {
+        // write your code here
+    }
+}
