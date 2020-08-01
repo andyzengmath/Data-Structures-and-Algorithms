a32248cc32ffb8338eaad92b7cde1e2c4f5782e0
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-01 12:10:57 -0400

    Update LintCode_138.java

--- Lintcode/Binary search tree&Hash table/138. Subarray Sum/LintCode_138.java
+++ Lintcode/Binary search tree&Hash table/138. Subarray Sum/LintCode_138.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 public class LintCode_138 {
     /**
      * @param nums: A list of integers
@@ -5,5 +7,21 @@ public class LintCode_138 {
      */
     public List<Integer> subarraySum(int[] nums) {
         // write your code here
+        List<Integer> result = new ArrayList<>();
+
+        Map<Integer, Integer> sumMap = new HashMap<>();
+        sumMap.put(0, -1);
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sumMap.containsKey(sum)) {
+                result.add(sumMap.get(sum) + 1);
+                result.add(i);
+                break;
+            }
+            sumMap.put(sum, i);
+        }
+        return result;
     }
 }
