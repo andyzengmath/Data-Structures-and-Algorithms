177b53dce96b1c889f1cfc299aeb5006af9dc35e
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-01 10:53:46 -0400

    Create LintCode_138.java

---
+++ Lintcode/Binary search tree&Hash table/138. Subarray Sum/LintCode_138.java
@@ -0,0 +1,9 @@
+public class LintCode_138 {
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number and the index of the last number
+     */
+    public List<Integer> subarraySum(int[] nums) {
+        // write your code here
+    }
+}
