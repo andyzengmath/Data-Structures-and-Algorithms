b2c7834168e75f864f8fd28ceab6d292641212be
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-01 10:14:40 -0400

    Update LintCode_95.java

--- Lintcode/Binary search tree&Hash table/95. Validate Binary Search Tree/LintCode_95.java
+++ Lintcode/Binary search tree&Hash table/95. Validate Binary Search Tree/LintCode_95.java
@@ -5,5 +5,20 @@ public class LintCode_95 {
      */
     public boolean isValidBST(TreeNode root) {
         // write your code here
+        return divideAndConquer(root, Long.MIN_VALUE, Long.MAX_VALUE);
+
+    }
+
+    private boolean divideAndConquer(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+
+        return divideAndConquer(root.left, min, Math.min(max, root.val)) &&
+                divideAndConquer(root.right, Math.max(min, root.val), max);
     }
 }
