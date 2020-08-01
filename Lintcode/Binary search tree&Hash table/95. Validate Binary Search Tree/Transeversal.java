86dad594f5c40e93fc16c0e47972ce4f0e8ccd17
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-01 10:29:11 -0400

    Create Transeversal.java

---
+++ Lintcode/Binary search tree&Hash table/95. Validate Binary Search Tree/Transeversal.java
@@ -0,0 +1,21 @@
+public class Transeversal {
+    private int lastVal = Integer.MIN_VALUE;
+    private boolean firstNode = True;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        if (!firstNode && lastVal > root.val) {
+            return false;
+        }
+        firstNode = false;
+        lastVal = root.val;
+        if (!isValidBST(root.right)) {
+            return false;
+        }
+        return true;
+    }
+}
