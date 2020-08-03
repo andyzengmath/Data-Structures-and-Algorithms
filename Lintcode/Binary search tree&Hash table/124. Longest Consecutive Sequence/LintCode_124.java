ec3c09c99060841278cb83b401e1720cb400b8af
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-03 16:35:35 -0400

    Update LintCode_124.java
    minor debug

--- Lintcode/Binary search tree&Hash table/124. Longest Consecutive Sequence/LintCode_124.java
+++ Lintcode/Binary search tree&Hash table/124. Longest Consecutive Sequence/LintCode_124.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 public class LintCode_124 {
     /**
      * @param num: A list of integers
@@ -6,5 +8,31 @@ public class LintCode_124 {
      */
     public int longestConsecutive(int[] num) {
         // write your code here
+        if (num == null || num.length == 0) {
+            return 0;
+        }
+
+        Map<Integer, Integer> list = new HashMap<>();
+
+        int maxLength = 0;
+        for (int n: num) {
+            if (list.containsKey(n)) {
+                continue;
+            }
+            int leftCount = 0, rightCount = 0;
+            if (list.containsKey(n - 1)) {
+                leftCount = list.get(n - 1);
+            } 
+            if (list.containsKey(n + 1)) {
+                rightCount = list.get(n + 1);
+            }
+            int count = leftCount + rightCount + 1;
+            maxLength = Math.max(maxLength, count);
+            list.put(n, count);
+            list.put(n - leftCount, count);
+            list.put(n + rightCount, count);
+        }
+
+        return maxLength;
     }
 }
