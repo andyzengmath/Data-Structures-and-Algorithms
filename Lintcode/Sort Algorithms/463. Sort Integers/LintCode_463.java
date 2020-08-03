90ffd37508df5e83405c76b0ec3ab6411384d9e0
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-03 19:52:24 -0400

    Update LintCode_463.java
    Insertion sort

--- Lintcode/Sort Algorithms/463. Sort Integers/LintCode_463.java
+++ Lintcode/Sort Algorithms/463. Sort Integers/LintCode_463.java
@@ -5,5 +5,15 @@ public class LintCode_463 {
      */
     public void sortIntegers(int[] A) {
         // write your code here
+        // insertion sort
+        for (int j = 1; j < A.length; j++) {
+            int key = A[j];
+            int i = j - 1;
+            while (i >= 0 && A[i] > key) {
+                A[i + 1] = A[i];
+                i = i - 1;
+            }
+            A[i + 1] = key;
+        }
     }
 }
