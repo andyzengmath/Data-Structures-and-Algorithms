6377b9150b07c3974a894682543d29fa1f22c6cd
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-04 21:34:59 -0400

    Update LintCode_173.java

--- Lintcode/Sort Algorithms/173. Insertion Sort List/LintCode_173.java
+++ Lintcode/Sort Algorithms/173. Insertion Sort List/LintCode_173.java
@@ -1,3 +1,9 @@
 public class LintCode_173 {
-    
+    /**
+     * @param head: The first node of linked list.
+     * @return: The head of linked list.
+     */
+    public ListNode insertionSortList(ListNode head) {
+        // write your code here
+    }
 }
