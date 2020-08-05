c6cd24d56b9ec99ea722a49409d655a5ff949dce
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-04 20:10:00 -0400

    Create LintCode_156.java

---
+++ Lintcode/Sort Algorithms/156. Merge Intervals/LintCode_156.java
@@ -0,0 +1,20 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+public class LintCode_156 {
+    /**
+     * @param intervals: interval list.
+     * @return: A new interval list.
+     */
+    public List<Interval> merge(List<Interval> intervals) {
+        // write your code here
+        
+    }
+}
