97309ba63bbad09ba14116413043412e3647f8fd
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-04 21:26:49 -0400

    Update LintCode_156.java

--- Lintcode/Sort Algorithms/156. Merge Intervals/LintCode_156.java
+++ Lintcode/Sort Algorithms/156. Merge Intervals/LintCode_156.java
@@ -12,9 +12,36 @@ public class LintCode_156 {
     /**
      * @param intervals: interval list.
      * @return: A new interval list.
+     * O(n log n) time and O(1) extra space.
+
      */
     public List<Interval> merge(List<Interval> intervals) {
         // write your code here
+        if (intervals.size() == 0) {
+            return intervals;
+        } 
         
+        intervals.sort(Comparator.comparing(i -> i.start));
+        List<Interval> result = new ArrayList<Interval>();
+        Interval lastInterval = intervals.get(0);
+
+        for (Interval interval: intervals) {
+            if (haveIntercation(lastInterval, interval)) {
+                lastInterval = mergeTwoIntervals(lastInterval, interval);
+            } else {
+                result.add(lastInterval);
+                lastInterval = interval;
+            }
+        }
+        result.add(lastInterval);
+        
+        return result;
+    }
+
+    private Interval mergeTwoIntervals(Interval a, Interval b) {
+        return new Interval(Math.min(a.start, b.start), Math.max(a.end, b.end));
+    }
+    private boolean haveIntercation(Interval a, Interval b) {
+        return Math.max(a.start, b.start) <= Math.min(a.end, b.end);
     }
 }
