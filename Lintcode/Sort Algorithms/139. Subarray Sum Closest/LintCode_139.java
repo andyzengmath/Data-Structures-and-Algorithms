22a79b786c7ae1d3ef2a2f42e81d108f3980bae8
Andy Zeng <acjzqy@gmail.com>
2020-08-07 16:54:20 -0400

    Update LintCode_139.java

---
+++ Lintcode/Sort Algorithms/139. Subarray Sum Closest/LintCode_139.java
@@ -0,0 +1,11 @@
+public class LintCode_139 {
+    /*
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number and the index of the last number
+     * Given an integer array, find a subarray with sum closest to zero. Return the indexes of the first number and last number.
+     * O(nlogn) time
+     */
+    public int[] subarraySumClosest(int[] nums) {
+        // write your code here
+    }
+}
