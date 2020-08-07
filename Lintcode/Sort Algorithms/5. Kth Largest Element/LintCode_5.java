062ce0333babc31814a7b49893e39b02e1a05ac4
Andy Zeng <acjzqy@gmail.com>
2020-08-07 19:50:26 -0400

    Update LintCode_5.java

--- Lintcode/Sort Algorithms/5. Kth Largest Element/LintCode_5.java
+++ Lintcode/Sort Algorithms/5. Kth Largest Element/LintCode_5.java
@@ -7,5 +7,33 @@ public class LintCode_5 {
      */
     public int kthLargestElement(int n, int[] nums) {
         // write your code here
+        // quick select
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int index = left - 1;
+            for (int i = left; i < right; i++) {
+                if (nums[i] > nums[right]) {
+                    swap(nums, i, ++index);
+                }
+            }
+            swap(nums, ++index, right);
+            if (index == n - 1) {
+                return nums[index];
+            }
+            if (index < n - 1) {
+                left = index + 1;
+            } else {
+                right = index - 1;
+            }
+
+        }
+        return -1;
+
+    }
+
+    public void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
