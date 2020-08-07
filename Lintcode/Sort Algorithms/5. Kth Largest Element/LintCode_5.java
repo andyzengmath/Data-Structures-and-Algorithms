15163f73b46514ab0bcc31bc00a08952b4f9f500
Andy Zeng <acjzqy@gmail.com>
2020-08-07 19:26:33 -0400

    Create LintCode_5.java

---
+++ Lintcode/Sort Algorithms/5. Kth Largest Element/LintCode_5.java
@@ -0,0 +1,11 @@
+public class LintCode_5 {
+    **
+     * @param n: An integer
+     * @param nums: An array
+     * @return: the Kth largest element
+     * Challenge: O(n) time, O(1) extra memory.
+     */
+    public int kthLargestElement(int n, int[] nums) {
+        // write your code here
+    }
+}
