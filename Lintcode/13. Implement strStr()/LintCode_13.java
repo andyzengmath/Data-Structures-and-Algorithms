4eef8ee237853af4ccc0980f7a5d7421efcbe677
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-10 06:26:01 -0400

    Update LintCode_13.java

--- Lintcode/13. Implement strStr()/LintCode_13.java
+++ Lintcode/13. Implement strStr()/LintCode_13.java
@@ -7,6 +7,18 @@ public class LintCode_13 {
      */
     public int strStr(String source, String target) {
         // Write your code here
+        if (target.equals("")) {
+            return 0;
+        }
+        for (int i = 0; i < source.length(); i++) {
+            for (int j = i; j < source.length(); j++) {
+                String str = source.substring(i, j + 1);
+                if (str.equals(target)) {
+                    return i;
+                }
+            }
+        }
         
+        return -1;
     }
 }
