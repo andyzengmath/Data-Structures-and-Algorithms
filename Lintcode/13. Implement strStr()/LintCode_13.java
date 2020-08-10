96ebef5c01bd4138eb2d05fd95fdd342f2a2b18c
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-10 06:19:30 -0400

    Create LintCode_13.java

---
+++ Lintcode/13. Implement strStr()/LintCode_13.java
@@ -0,0 +1,12 @@
+public class LintCode_13 {
+    /**
+     * @param source: 
+     * @param target: 
+     * @return: return the index
+     * O(n2) is acceptable. Can you implement an O(n) algorithm? (hint: KMP)
+     */
+    public int strStr(String source, String target) {
+        // Write your code here
+        
+    }
+}
