f1cd4433bb46eb6b45f2d7bfa7df79811733cc4e
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-11 18:21:54 -0400

    Update LintCode_41.java

--- Lintcode/41. Maximum Subarray/LintCode_41.java
+++ Lintcode/41. Maximum Subarray/LintCode_41.java
@@ -6,5 +6,18 @@ public class LintCode_41 {
      */
     public int maxSubArray(int[] nums) {
         // write your code here
+        // prefix sum
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int max = Integer.MIN_VALUE, sum = 0, minSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum - minSum);
+            minSum = Math.min(sum, minSum);
+        }
+
+        return max;
     }
 }
