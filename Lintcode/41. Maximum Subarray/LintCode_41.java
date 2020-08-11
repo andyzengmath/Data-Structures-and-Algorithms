461853de2490421eccbbf4e53fe9df7ba48d4c2d
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-11 17:21:49 -0400

    Create LintCode_41.java

---
+++ Lintcode/41. Maximum Subarray/LintCode_41.java
@@ -0,0 +1,10 @@
+public class LintCode_41 {
+    /**
+     * @param nums: A list of integers
+     * @return: A integer indicate the sum of max subarray
+     * Can you do it in time complexity O(n)?
+     */
+    public int maxSubArray(int[] nums) {
+        // write your code here
+    }
+}
