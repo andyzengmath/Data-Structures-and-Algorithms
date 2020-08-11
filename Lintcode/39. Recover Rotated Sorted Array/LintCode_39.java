34105fdee0447cd5b76e70b68596e781a3712543
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-11 17:03:41 -0400

    Update LintCode_39.java

--- Lintcode/39. Recover Rotated Sorted Array/LintCode_39.java
+++ Lintcode/39. Recover Rotated Sorted Array/LintCode_39.java
@@ -6,5 +6,24 @@ public class LintCode_39 {
      */
     public void recoverRotatedSortedArray(List<Integer> nums) {
         // write your code here
+        int cur = 0;
+        int len = nums.size() - 1;
+        while (cur < len) {
+            if (nums.get(cur) <= nums.get(cur + 1)) {
+                cur ++;
+            } else {
+                break;
+            }
+        }
+
+        if (cur == len) {
+            return;
+        }
+
+        for (int i = cur + 1; i < len; i ++) {
+            int val = nums.get(i);
+            nums.remove(i);
+            nums.add(i - cur - 1, val);
+        }
     }
 }
