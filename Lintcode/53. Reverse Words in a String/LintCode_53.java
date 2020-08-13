57b647d54ce26d95aab6d979df84aba9fe66505d
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-13 06:44:20 -0400

    Update LintCode_53.java

--- Lintcode/53. Reverse Words in a String/LintCode_53.java
+++ Lintcode/53. Reverse Words in a String/LintCode_53.java
@@ -5,5 +5,23 @@ public class LintCode_53 {
      */
     public String reverseWords(String s) {
         // write your code here
+        Stack<String> stack = new Stack<>();
+        String temp = "";
+        for (int i = 0; i < s.length; i++) {
+            if (s.charAt(i) == ' ' && !temp.equals("")) {
+                stack.push(temp);
+                temp = "";
+            } else if (s.charAt(i) != ' ') {
+                temp += s.charAt(i);
+            }
+        }
+
+        stack.push(temp);
+        String result = "";
+        while (!stack.isEmpty()) {
+            result += stack.pop() + " ";
+        }
+        result = result.substring(0, result.length() - 1);
+        return result;
     }
 }
