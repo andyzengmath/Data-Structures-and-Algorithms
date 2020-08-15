1c2d4ab0e06b5039e210aba9bd95894f017f6a33
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-15 06:17:33 -0400

    Create LintCode_67.java

---
+++ Lintcode/67. Binary Tree Inorder Traversal/LintCode_67.java
@@ -0,0 +1,9 @@
+public class LintCode_67 {
+    /**
+     * @param root: A Tree
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // write your code here
+    }
+}
