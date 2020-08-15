bd994b0bbfa9759c49918b256ec9028c4f4d8a87
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-15 06:29:22 -0400

    Update LintCode_67.java

--- Lintcode/67. Binary Tree Inorder Traversal/LintCode_67.java
+++ Lintcode/67. Binary Tree Inorder Traversal/LintCode_67.java
@@ -5,5 +5,37 @@ public class LintCode_67 {
      */
     public List<Integer> inorderTraversal(TreeNode root) {
         // write your code here
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.peek();
+            result.add(curr.val);
+
+            if (curr.right == null) {
+                curr = stack.pop();
+                while (!stack.isEmpty() && stack.peek().right == curr) {
+                    curr = stack.pop();
+                }
+            } else {
+                curr = curr.right;
+                while (curr != null) {
+                    stack.push(curr);
+                    curr = curr.left;
+                }
+            }
+            
+        }
+
+        return result;
     }
 }
