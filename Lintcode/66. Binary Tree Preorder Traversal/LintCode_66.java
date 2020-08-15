223fc7b0ca6eb92226422d4e305f5c2a56b70822
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-14 21:36:04 -0400

    Create LintCode_66.java

---
+++ Lintcode/66. Binary Tree Preorder Traversal/LintCode_66.java
@@ -0,0 +1,9 @@
+public class LintCode_66 {
+    /**
+     * @param root: A Tree
+     * @return: Preorder in ArrayList which contains node values.
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        // write your code here
+    }
+}
