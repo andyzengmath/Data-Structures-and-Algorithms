5f56f64f100ad4facff904aadc04298b2ad6649c
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-15 09:47:42 -0400

    Create LintCode_80.java

---
+++ Lintcode/80. Median/LintCode_80.java
@@ -0,0 +1,9 @@
+public class LintCode_80 {
+    /**
+     * @param nums: A list of integers
+     * @return: An integer denotes the middle number of the array
+     */
+    public int median(int[] nums) {
+        // write your code here
+    }
+}
