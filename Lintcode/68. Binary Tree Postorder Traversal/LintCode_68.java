64762e79a1b45a48600e1dd0d4ecc1984f9f8adc
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-14 21:58:28 -0400

    Create LintCode_68.java

---
+++ Lintcode/68. Binary Tree Postorder Traversal/LintCode_68.java
@@ -0,0 +1,9 @@
+public class LintCode_68 {
+    /**
+     * @param root: A Tree
+     * @return: Postorder in ArrayList which contains node values.
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        // write your code here
+    }
+}
