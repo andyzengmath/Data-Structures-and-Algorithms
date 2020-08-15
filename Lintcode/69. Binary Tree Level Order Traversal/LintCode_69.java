29bb81d19d94a6e1aabc63783ac42c297eb5a2b0
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-15 06:55:59 -0400

    Create LintCode_69.java

---
+++ Lintcode/69. Binary Tree Level Order Traversal/LintCode_69.java
@@ -0,0 +1,9 @@
+public class LintCode_69 {
+    /**
+     * @param root: A Tree
+     * @return: Level order a list of lists of integer
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // write your code here
+    }
+}
