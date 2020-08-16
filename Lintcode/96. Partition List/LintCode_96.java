b3bc1e9541475914d64c0d2d092363c4ef6dfc20
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-16 07:49:55 -0400

    Update LintCode_96.java

--- Lintcode/96. Partition List/LintCode_96.java
+++ Lintcode/96. Partition List/LintCode_96.java
@@ -6,5 +6,27 @@ public class LintCode_96 {
      */
     public ListNode partition(ListNode head, int x) {
         // write your code here
+        // double pointer
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode leftDummy = new ListNode(0);
+        ListNode rightDummy = new ListNode(0);
+        ListNode left = leftDummy, right = rightDummy;
+        while (head != null) {
+            if (head.val < x) {
+                left.next = head;
+                left = head;
+            } else {
+                right.next = head;
+                right = head;
+            }
+            head = head.next;
+        }
+        right.next = null;
+        left.next = rightDummy.next;
+        return leftDummy.next;
+
     }
 }
