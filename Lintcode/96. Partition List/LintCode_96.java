b7ecf3d3b078086b9e8a19cf2fdf1eee6aa60547
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-16 07:36:32 -0400

    Create LintCode_96.java

---
+++ Lintcode/96. Partition List/LintCode_96.java
@@ -0,0 +1,10 @@
+public class LintCode_96 {
+    /**
+     * @param head: The first node of linked list
+     * @param x: An integer
+     * @return: A ListNode
+     */
+    public ListNode partition(ListNode head, int x) {
+        // write your code here
+    }
+}
