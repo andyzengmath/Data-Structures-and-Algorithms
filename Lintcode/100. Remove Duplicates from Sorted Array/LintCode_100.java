bf0a518cf9a92abb1769cdbe0cdd4c5e25c24df6
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-16 07:51:47 -0400

    Create LintCode_100.java

---
+++ Lintcode/100. Remove Duplicates from Sorted Array/LintCode_100.java
@@ -0,0 +1,9 @@
+public class LintCode_100 {
+    /*
+     * @param nums: An ineger array
+     * @return: An integer
+     */
+    public int removeDuplicates(int[] nums) {
+        // write your code here
+    }
+}
