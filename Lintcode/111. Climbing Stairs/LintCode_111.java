b4f72c27b4c0c5791b23481481b60e312eccdf47
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-17 07:38:36 -0400

    Create LintCode_111.java

---
+++ Lintcode/111. Climbing Stairs/LintCode_111.java
@@ -0,0 +1,21 @@
+public class LintCode_111 {
+    /**
+     * @param n: An integer
+     * @return: An integer
+     */
+    public int climbStairs(int n) {
+        // write your code here
+        if (n == 0 ) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        int prev = 0, curr = 1;
+        for (int i = 0; i < n - 1; i++) {
+            int temp = prev + curr;
+            prev = curr;
+            curr = temp;
+        }
+        return curr;
+    }
+}
