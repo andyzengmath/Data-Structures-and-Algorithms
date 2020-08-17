5bcfc4d7a55975e5eed91dd25f089807746db8c1
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-17 08:01:29 -0400

    Create combinatorial.java

---
+++ Lintcode/114. Unique Paths/combinatorial.java
@@ -0,0 +1,26 @@
+public class combinatorial {
+    public int uniquePaths(int m, int n) {
+        if (m == 0 || n == 0) {
+            return 0;
+        } else if (m == 1 || n == 1) {
+            return 1;
+        }
+
+        if (m > n) {
+            int temp = n;
+            n = m;
+            m = temp;
+        }
+
+        double temp = 1;
+        double result = 1;
+        for (int i = 1; i <= m - 1; i++) {
+            temp *= i;
+        }
+        for (int j = n; j <= m + n - 2, j++) {
+            result *= i;
+        }
+        result = result / temp;
+        return (int)Math.round(result);
+    }
+}
