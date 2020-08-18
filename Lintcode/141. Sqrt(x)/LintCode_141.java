c543aa54ba7e4bd43a85e5c7becfa45081035def
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-18 11:23:56 -0400

    Update LintCode_141.java

--- Lintcode/141. Sqrt(x)/LintCode_141.java
+++ Lintcode/141. Sqrt(x)/LintCode_141.java
@@ -5,5 +5,26 @@ public class LintCode_141 {
      */
     public int sqrt(int x) {
         // write your code here
+        // first try: binary search
+        if (x < 0) throw new IllegalArgumentException();
+        else if (x <= 1) {
+            return x;
+        }
+        int start = 1, end = x;
+        while (start + 1 < end) {
+            int mid = (start + end) / 2;
+            if (mid == x / mid) {
+                return mid; // note: mid * mid = x might cause overflow
+            } else if (mid < x / mid) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+
+        }
+        if (end > x / end) {
+            return start;
+        }
+        return end;
     }
 }
