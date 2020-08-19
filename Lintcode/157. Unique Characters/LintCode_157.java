7bc491df73bfdb15ef4ede3e3bbb4725351c6526
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-19 07:16:07 -0400

    Create LintCode_157.java

---
+++ Lintcode/157. Unique Characters/LintCode_157.java
@@ -0,0 +1,10 @@
+public class LintCode_157 {
+    /*
+     * @param str: A string
+     * @return: a boolean
+     * What if you can not use additional data structures?
+     */
+    public boolean isUnique(String str) {
+        // write your code here
+    }
+}
