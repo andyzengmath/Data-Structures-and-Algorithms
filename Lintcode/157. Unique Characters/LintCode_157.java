ca54f736852f52ca5de79c83ea886328485aefa8
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-19 07:19:08 -0400

    Update LintCode_157.java

--- Lintcode/157. Unique Characters/LintCode_157.java
+++ Lintcode/157. Unique Characters/LintCode_157.java
@@ -6,5 +6,17 @@ public class LintCode_157 {
      */
     public boolean isUnique(String str) {
         // write your code here
+        int len = str.length();
+        if (len == 0 || len == 1) {
+            return true;
+        }
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
