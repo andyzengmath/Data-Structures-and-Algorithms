70c07812cf0fa6a23fb13a8f669d047320cd99e8
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-19 09:39:53 -0400

    Create LintCode_167.java

---
+++ Lintcode/167. Add Two Numbers/LintCode_167.java
@@ -0,0 +1,12 @@
+public class LintCode_167 {
+    /**
+     * Frequently asked question ++
+     * Airbnb,Amazon,Microsoft,Bloomberg,Adobe
+     * @param l1: the first list
+     * @param l2: the second list
+     * @return: the sum list of l1 and l2 
+     */
+    public ListNode addLists(ListNode l1, ListNode l2) {
+        // write your code here
+    }
+}
