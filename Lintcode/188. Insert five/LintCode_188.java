b33d24a562665bf0c9c476895824da5686763ebb
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-20 10:54:41 -0400

    Create LintCode_188.java

---
+++ Lintcode/188. Insert five/LintCode_188.java
@@ -0,0 +1,9 @@
+public class LintCode_188 {
+    /**
+     * @param a: A number
+     * @return: Returns the maximum number after insertion
+     */
+    public int InsertFive(int a) {
+        // write your code here
+    }
+}
