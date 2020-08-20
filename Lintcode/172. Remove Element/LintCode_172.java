e1968688abeb523ced5c689e5608399db0ddf8ee
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-20 07:48:11 -0400

    Create LintCode_172.java

---
+++ Lintcode/172. Remove Element/LintCode_172.java
@@ -0,0 +1,11 @@
+public class LintCode_172 {
+    /* Frequently asked question ++
+     * @param A: A list of integers
+     * @param elem: An integer
+     * @return: The new length after remove
+     */
+    public int removeElement(int[] A, int elem) {
+        // write your code here
+        
+    }
+}
