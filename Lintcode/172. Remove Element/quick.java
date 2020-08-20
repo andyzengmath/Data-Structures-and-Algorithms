c30740f5c7f2bb6b304ab040aad08a40c5522c38
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-20 07:57:34 -0400

    Create quick.java

---
+++ Lintcode/172. Remove Element/quick.java
@@ -0,0 +1,25 @@
+public class quick {
+    public int removeElement(int[] A, int elem) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int i = 0, j = A.length - 1;
+        while (i <= j) {
+            while (i <= j && A[i] != elem) {
+                i++;
+            }
+            while (i <= j && A[j] == elem) {
+                j--;
+            }
+            while (i <= j) {
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        return i;
+    }
+
+}
