0a351d78468b2fdc4849054b926fd3d341a7ad89
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-21 08:06:36 -0400

    Update LintCode_193.java

--- Lintcode/193. Longest Valid Parentheses/LintCode_193.java
+++ Lintcode/193. Longest Valid Parentheses/LintCode_193.java
@@ -4,6 +4,23 @@ public class LintCode_193 {
      * @return: return a integer
      */
     public int longestValidParentheses(String s) {
-        // write your code here
+        // write your code here\
+        if (s == null || s.length() <= 1) {
+            return 0;
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                if (stack.isEmpty() || s.charAt(stack.peek()) == ')') {
+                    stack.push(i);
+                } else {
+                    stack.pop();
+                    max = Math.max(max, i - (stack.isEmpty() ? -1 : stack.peek()));
+                }
+            }
+        }
+        return max;
     }
 }
