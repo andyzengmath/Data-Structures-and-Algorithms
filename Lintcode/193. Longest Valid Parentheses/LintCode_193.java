4ad9e5a3a2d11fb15647df23d3c2d44f5ff2fb18
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-21 07:29:37 -0400

    Create LintCode_193.java

---
+++ Lintcode/193. Longest Valid Parentheses/LintCode_193.java
@@ -0,0 +1,9 @@
+public class LintCode_193 {
+    /**
+     * @param s: a string
+     * @return: return a integer
+     */
+    public int longestValidParentheses(String s) {
+        // write your code here
+    }
+}
