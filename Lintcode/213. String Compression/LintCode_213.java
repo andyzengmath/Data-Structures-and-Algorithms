13259511383d3aa5b8a3d4a18ffffe2a719e93d8
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-22 08:30:17 -0400

    Create LintCode_213.java

---
+++ Lintcode/213. String Compression/LintCode_213.java
@@ -0,0 +1,9 @@
+public class LintCode_213 {
+    /**
+     * @param originalString: a string
+     * @return: a compressed string
+     */
+    public String compress(String originalString) {
+        // write your code here
+    }
+}
