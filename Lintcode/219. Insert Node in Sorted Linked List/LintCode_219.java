446499ab32779d293bb66a82a6884d49ba0310dd
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-22 10:45:36 -0400

    Update LintCode_219.java

--- Lintcode/219. Insert Node in Sorted Linked List/LintCode_219.java
+++ Lintcode/219. Insert Node in Sorted Linked List/LintCode_219.java
@@ -1,3 +1,22 @@
 public class LintCode_219 {
-    
+    /**
+     * @param head: The head of linked list.
+     * @param val: An integer.
+     * @return: The head of new linked list.
+     */
+    public ListNode insertNode(ListNode head, int val) {
+        // write your code here
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy
+        
+        while (cur.next != null && cur.next.val < val) {
+            cur = cur.next;
+        }
+        ListNode new = new ListNode(val);
+        new.next = cur.next;
+        cur.next = new;
+
+        return dummy.next;
+    }
 }
