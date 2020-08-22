40b90049a5c5be3aac65be05225f66e0a8ebc11e
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-22 10:26:09 -0400

    Update LintCode_213.java

--- Lintcode/213. String Compression/LintCode_213.java
+++ Lintcode/213. String Compression/LintCode_213.java
@@ -5,5 +5,26 @@ public class LintCode_213 {
      */
     public String compress(String originalString) {
         // write your code here
+        if (originalString == null || originalString.length() == 0) {
+            return originalString;
+        }
+
+        int i = 0, j = 0;
+        StringBuilder sb = new StringBuilder();
+        while (j < originalString.length()) {
+            if (originalString.charAt(i) != originalString.charAt(j)) {
+                sb.append(str.charAt(i));
+                sb.append(j - i);
+                i = j;
+            }
+            j++;
+        }
+        sb.append(str.charAt(i));
+        sb.append(j - i);
+
+        if (sb.length >= originalString.length()) {
+            return originalString;
+        }
+        return sb.toString();
     }
 }
