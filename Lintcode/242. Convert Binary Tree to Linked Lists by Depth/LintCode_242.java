2b1fc6e34edcec585cc6bd2d39a230529f2b8646
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-23 09:30:33 -0400

    Create LintCode_242.java

---
+++ Lintcode/242. Convert Binary Tree to Linked Lists by Depth/LintCode_242.java
@@ -0,0 +1,27 @@
+public class LintCode_242 {
+    /**
+     * @param root the root of binary tree
+     * @return a lists of linked list
+     */
+    public List<ListNode> binaryTreeToLists(TreeNode root) {
+        // Write your code here
+    }
+    
+}
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
