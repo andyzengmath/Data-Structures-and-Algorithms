e4bd7de35112611cfe29285ceed7a5b0b0e2dda7
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-23 12:09:03 -0400

    Create LintCode_254.java

---
+++ Lintcode/254. Drop Eggs/LintCode_254.java
@@ -0,0 +1,9 @@
+public class LintCode_254 {
+    /** don't understand
+     * @param n: An integer
+     * @return: An integer
+     */
+    public int dropEggs(int n) {
+        // write your code here
+    }
+}
