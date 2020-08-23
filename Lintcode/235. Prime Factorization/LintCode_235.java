01c8d4397b8031a735d318de409b28fb4cbefc96
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-23 09:17:28 -0400

    Update LintCode_235.java

--- Lintcode/235. Prime Factorization/LintCode_235.java
+++ Lintcode/235. Prime Factorization/LintCode_235.java
@@ -1,3 +1,21 @@
 public class LintCode_235 {
-    
+    /** Uber
+     * @param num: An integer
+     * @return: an integer array
+     */
+    public List<Integer> primeFactorization(int num) {
+        // write your code here
+        List<Integer> results = new ArrayList<Integer>();
+        int numSqrt = (int) Math.sqrt(num);
+        for (int i = 2; i <= numSqrt; i++) {
+            while (nums % i == 0) {
+                results.add(i);
+                nums /= i;
+            }
+        }
+        if (nums > 1) {
+            results.add(nums);
+        }
+        return results;
+    }
 }
