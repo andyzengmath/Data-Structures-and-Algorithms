810f3b1150fb89ffc8576757c75c0daf6be6ef43
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-23 10:28:00 -0400

    Create LintCode_250.java

---
+++ Lintcode/250. Special Palindrome String/LintCode_250.java
@@ -0,0 +1,10 @@
+public class LintCode_250 {
+    /**
+     * @param ambigram: A list of paired ambigram letter.
+     * @param word: A string need to be judged.
+     * @return: If it is special palindrome string, return true.
+     */
+    public boolean ispalindrome(List<String> ambigram, String word) {
+        // write your code here.
+    }
+}
