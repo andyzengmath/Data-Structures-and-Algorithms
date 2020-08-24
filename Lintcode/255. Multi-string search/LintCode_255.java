521285549a6bb5392c6ff1b08a9452c2e5b1f675
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-24 08:08:14 -0400

    Create LintCode_255.java

---
+++ Lintcode/255. Multi-string search/LintCode_255.java
@@ -0,0 +1,10 @@
+public class LintCode_255 {
+    /**
+     * @param sourceString: a string
+     * @param targetStrings: a string array
+     * @return: Returns a bool array indicating whether each string in targetStrings is a substring of the sourceString
+     */
+    public boolean[] whetherStringsAreSubstrings(String sourceString, String[] targetStrings) {
+        // write your code here
+    }
+}
