a747e336dc9caf81e485be7852f922c5ca061ba2
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-24 08:38:17 -0400

    Update LintCode_260.java

--- Lintcode/260. Same Diagonal Elements/LintCode_260.java
+++ Lintcode/260. Same Diagonal Elements/LintCode_260.java
@@ -9,7 +9,7 @@ public class LintCode_260 {
             return true;
         }
         int n = matrix.length;
-        for (int i = 1); i <= n - 1; i++) {
+        for (int i = 1; i <= n - 1; i++) {
             for (int j = 1; j <= n - 1; j++) {
                 if (matrix[i][j] != matrix[i - 1][j - 1]) {
                     return false;
