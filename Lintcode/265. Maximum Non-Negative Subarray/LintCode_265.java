e4a60039f513dfe52fadaa403131fe54648fbbfd
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-24 11:58:01 -0400

    Create LintCode_265.java

---
+++ Lintcode/265. Maximum Non-Negative Subarray/LintCode_265.java
@@ -0,0 +1,9 @@
+public class LintCode_265 {
+    /**
+     * @param A: an integer array
+     * @return: return maxium contiguous non-negative subarray sum
+     */
+    public int maxNonNegativeSubArray(int[] A) {
+        // write your code here
+    }
+}
