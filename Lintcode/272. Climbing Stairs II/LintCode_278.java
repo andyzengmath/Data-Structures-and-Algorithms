72fb5a95d6739ad4df99f04cc750fa511ea11fd4
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-24 12:21:02 -0400

    Create LintCode_278.java

---
+++ Lintcode/272. Climbing Stairs II/LintCode_278.java
@@ -0,0 +1,12 @@
+public class LintCode_278 {
+    /**
+     * @param screen: a two-dimensional array of colors
+     * @param x: the abscissa of the specified point
+     * @param y: the ordinate of the specified point
+     * @param newColor: the specified color
+     * @return: Can it be filled
+     */
+    public boolean paintFill(int[][] screen, int x, int y, int newColor) {
+        // write your code here.
+    }
+}
