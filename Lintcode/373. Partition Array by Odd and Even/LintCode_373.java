fab5e4ad3d89d07b533133ab5764d1707b9d1e91
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-25 07:31:39 -0400

    Create LintCode_373.java

---
+++ Lintcode/373. Partition Array by Odd and Even/LintCode_373.java
@@ -0,0 +1,32 @@
+public class LintCode_373 {
+    /* leetcode 905
+     * @param nums: an array of integers
+     * @return: nothing
+     */
+    public void partitionArray(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            while (start < end && nums[start] % 2 == 1) {
+                start++;
+            }
+            while (start < end && nums[end] % 2 == 0) {
+                end--;
+            }
+            while (start < end) {
+                swap(nums, start, end);
+                start++;
+                end--;
+            }
+        }
+    }
+
+    public static swap(int[] nums, int n, int m) {
+        int temp = nums[n];
+        nums[n] = nums[m];
+        nums[m] = temp;
+    }
+}
