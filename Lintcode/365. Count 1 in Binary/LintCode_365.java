446accd8c4064edd689bc1eb1bf6acbadac7e29a
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-25 07:19:09 -0400

    Update LintCode_365.java

--- Lintcode/365. Count 1 in Binary/LintCode_365.java
+++ Lintcode/365. Count 1 in Binary/LintCode_365.java
@@ -1,10 +1,16 @@
 public class LintCode_365 {
-    /*
+    /* LeetCode 191
      * @param num: An integer
      * @return: An integer
      * If the integer is n bits with m 1 bits. Can you do it in O(m) time?
      */
     public int countOnes(int num) {
         // write your code here
+        int count = 0;
+        while (num != 0) {
+            num = num & (num - 1);
+            count++;
+        }
+        return count;
     }
 }
