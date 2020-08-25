b553d8140204372cd1bae1d34d99103f0d10f0f1
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-25 06:32:54 -0400

    Update LintCode_278.java

--- Lintcode/272. Climbing Stairs II/LintCode_278.java
+++ Lintcode/272. Climbing Stairs II/LintCode_278.java
@@ -8,5 +8,22 @@ public class LintCode_278 {
      */
     public boolean paintFill(int[][] screen, int x, int y, int newColor) {
         // write your code here.
+        if (screen[x][y] == newColor) {
+            return false;
+        }
+        return paintFill(screen, x, y, screen[x][y], newColor);
+    }
+
+    public boolean paintFill(int[][] screen, int x, int y, int oldColor, int newColor) {
+        if (x < 0 || x >= screen.length || y < 0 || y > screen[0].length) return false;
+        if (screen[x][y] == oldColor) {
+            screen[x][y] = newColor;
+            paintFill(screen, x - 1, y, oldColor, newColor);
+            paintFill(screen, x + 1, y, oldColor, newColor);
+            paintFill(screen, x, y - 1, oldColor, newColor);
+            paintFill(screen, x, y + 1, oldColor, newColor);
+        }
+
+        return true;
     }
 }
