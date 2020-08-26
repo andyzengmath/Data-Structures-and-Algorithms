07a05022afa1bc7009a1d2c679ebbbf252615409
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-26 08:57:31 -0400

    Create LintCode_375.java

---
+++ Lintcode/375. Clone Binary Tree/LintCode_375.java
@@ -0,0 +1,9 @@
+public class LintCode_375 {
+    /**
+     * @param root: The root of binary tree
+     * @return: root of new tree
+     */
+    public TreeNode cloneTree(TreeNode root) {
+        // write your code here
+    }
+}
