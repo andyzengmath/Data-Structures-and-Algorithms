3fd99b69374a611dde424f22dc50312275f232de
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-26 09:11:31 -0400

    Update LintCode_375.java

--- Lintcode/375. Clone Binary Tree/LintCode_375.java
+++ Lintcode/375. Clone Binary Tree/LintCode_375.java
@@ -5,5 +5,28 @@ public class LintCode_375 {
      */
     public TreeNode cloneTree(TreeNode root) {
         // write your code here
+        if (root == null) {
+            return null;
+        }
+        TreeNode head = new TreeNode(root.val);
+        traverse(root, head);
+        return head;
+    }
+
+    private void traverse(TreeNode root, TreeNode head) {
+        if (root == null) {
+            return;
+        }
+        TreeNode cur = root;
+        if (cur.left != null) {
+            TreeNode left = new TreeNode(cur.left.val);
+            head.left = left;
+        }
+        if (cur.right != null) {
+            TreeNode right = new TreeNode(right.left.val);
+            head.right = right;
+        }
+        traverse(root.left, head.left);
+        traverse(root.right, head.right);
     }
 }
