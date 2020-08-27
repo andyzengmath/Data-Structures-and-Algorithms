b80e7934742e831a9e37062b426d7c25574eb117
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-27 07:24:46 -0400

    Create LeetCode_674.java

---
+++ LeetCode/674. Longest Continuous Increasing Subsequence/LeetCode_674.java
@@ -0,0 +1,21 @@
+public class LeetCode_674 {
+    public int findLengthOfLCIS(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int result = 1;
+        int countUp = 1;
+        
+
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] > A[i - 1]) {
+                countUp++;
+                result = Math.max(result, countUp); 
+            } else {
+                countUp = 1;
+            }
+        }
+       
+        return result;
+    }
+}
