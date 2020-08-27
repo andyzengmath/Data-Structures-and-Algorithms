e74c72f3fe45757266da83396bce8d66c2b7b3a6
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-27 08:20:36 -0400

    Update LintCode_408.java
    Total runtime 165 ms

--- 408. Add Binary/LintCode_408.java
+++ 408. Add Binary/LintCode_408.java
@@ -6,5 +6,34 @@ public class LintCode_408 {
      */
     public String addBinary(String a, String b) {
         // write your code here
+        if (a.length() < b.length()) {
+            String temp = a;
+            a = b;
+            b = temp;
+        }
+        int len1 = a.length() - 1;
+        int len2 = b.length() - 1;
+        int add = 0;
+        String result = "";
+
+        while (len2 >= 0) {
+            int sum = (int)(a.charAt(len1) - '0') + (int)(b.charAt(len2) - '0') + add;
+            result = String.valueOf(sum % 2) + result;
+            add = sum / 2;
+            len1--;
+            len2--;
+        }
+
+        while (len1 >= 0) {
+            int sum = (int)(a.charAt(len1) - '0') + add;
+            result = String.valueOf(sum % 2) + result;
+            add = sum / 2;
+            len1--;
+        }
+
+        if (add == 1) {
+            result = "1" + result;
+        }
+        return result;
     }
 }
