31382aaf9720a43bdae3cbf8981e378ea380f52f
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-27 08:04:17 -0400

    Create LintCode_408.java

---
+++ 408. Add Binary/LintCode_408.java
@@ -0,0 +1,10 @@
+public class LintCode_408 {
+    /**
+     * @param a: a number
+     * @param b: a number
+     * @return: the result
+     */
+    public String addBinary(String a, String b) {
+        // write your code here
+    }
+}
