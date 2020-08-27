6bc030a121f080b8cbe37349ef4b747ff2ed03be
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-27 09:11:59 -0400

    Update LintCode_417.java

--- Lintcode/417. Valid Number/LintCode_417.java
+++ Lintcode/417. Valid Number/LintCode_417.java
@@ -5,5 +5,46 @@ public class LintCode_417 {
      */
     public boolean isNumber(String s) {
         // write your code here
+        int i = 0, e = s.length() - 1;
+        while (i <= e && Character.isWhitespace(s.charAt(i))) {
+            i++;
+        }
+        if (i > e) {
+            return false;
+        }
+        while (e >= i && Charater.isWhitespace(s.charAt(e))) {
+            e--;
+        }
+
+        if(s.charAt(i) == '+' || s.charAt(i) == '-') {
+            i++;
+        }
+
+        boolean num = false;
+        boolean dot = false;
+        boolean exp = false;
+
+        while (i <= e) {
+            char c = s.charAt(i) {
+            if (Character.isDigit(c)) {
+                num = true;
+            } else if (c == '.') {
+                if (exp || num == false) {
+                    return false;
+                }
+                exp = true;
+                num = false;
+            } else if (c == '+' || c == '-') {
+                if (s.charAt(i - 1) != e) {
+                    return false;
+                } 
+            } else {
+                return false;
+            }
+            i++;
+            }
+        }
+        return num;
+
     }
 }
