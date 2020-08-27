c69f72f321e0b4611ad8c09e6066da9cee72c0b9
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-27 08:29:34 -0400

    add 417

---
+++ Lintcode/417. Valid Number/LintCode_417.java
@@ -0,0 +1,9 @@
+public class LintCode_417 {
+    /**
+     * @param s: the string that represents a number
+     * @return: whether the string is a valid number
+     */
+    public boolean isNumber(String s) {
+        // write your code here
+    }
+}
