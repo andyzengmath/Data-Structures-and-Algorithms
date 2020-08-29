b4520d8ba40ad04107553edc3d0131c16f1cf6f6
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-29 12:20:05 -0400

    Create LintCode_468.java

---
+++ Lintcode/468. Symmetric Binary Tree/LintCode_468.java
@@ -0,0 +1,28 @@
+public class LintCode_468 {
+    /**
+     * @param root: the root of binary tree.
+     * @return: true if it is a mirror of itself, or false.
+     */
+    public boolean isSymmetric(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return true;
+        }
+        return check(root.left, root.right);
+    }
+
+    private boolean check(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+
+        if (root.val != root2.val) {
+            return false;
+        }
+
+        return check(root1.left, root2.right) && check(root1.right, root2.left);
+    }
+}
