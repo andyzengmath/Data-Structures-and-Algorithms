cfa67682aa496c60d831419031f5be712a99c0f7
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-29 08:07:53 -0400

    Create LintCode_457.java

---
+++ Lintcode/457. Classical Binary Search/LintCode_457.java
@@ -0,0 +1,28 @@
+public class LintCode_457 {
+    /**
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return: An integer
+     */
+    public int findPosition(int[] nums, int target) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0, end = nums.length - 1;
+        while ( start < end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        if (nums[start] == target) {
+            return start;
+        }
+        return -1;
+    }
+}
