f206077238253b7524ca2c8478c5ebed19b06f85
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-30 06:57:50 -0400

    add two pointer

--- Lintcode/474. Lowest Common Ancestor II/twoPointer.java
+++ Lintcode/474. Lowest Common Ancestor II/twoPointer.java
@@ -1,6 +1,6 @@
 public class twoPointer {
     public ParentTreeNode lowestCommonAncestorII(ParentTreeNode root, ParentTreeNode A, ParentTreeNode B) {
-        ParentTreenNode p1 = A, p2 = A.parent;
+        ParentTreeNode p1 = A, p2 = B;
         while (p1 != p2) {
             p1 = p1.parent == null ? B : p1.parent;
             p2 = p2.parent == null ? A : p2.parent;
