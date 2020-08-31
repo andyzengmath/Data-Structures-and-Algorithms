3c5ef552833d0bc133fd5c86c624a81f2862f42b
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-08-30 23:16:16 -0400

    Create LintCode_488.java

---
+++ Lintcode/488. Happy Number/LintCode_488.java
@@ -0,0 +1,27 @@
+public class LintCode_488 {
+    /**
+     * @param n: An integer
+     * @return: true if this is a happy number or false
+     */
+    public boolean isHappy(int n) {
+        // write your code here
+        HashSet<Integer> hash = new HashSet<Integer>();
+        while (n != 1) {
+            if (hash.contains(n)) {
+                return false;
+            }
+            hash.add(n);
+            n = getNextHappy(n);
+        }
+        return true;
+    }
+    
+    private int getNextHappy(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
+    }
+}
