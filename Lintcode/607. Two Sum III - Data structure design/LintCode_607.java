fa51e242a2247b7e92fafb88fe770c058a7916d4
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-09-02 12:17:52 -0400

    Create LintCode_607.java
    Total runtime 1641 ms
    Your submission beats 99.00% Submissions!

---
+++ Lintcode/607. Two Sum III - Data structure design/LintCode_607.java
@@ -0,0 +1,38 @@
+import java.util.Map;
+
+public class LintCode_607 {
+    /**
+     * @param number: An integer
+     * @return: nothing
+     */
+    private List<Integer> list = null;
+    private Map<Integer, Integer> map = null;
+    public TwoSum() {
+        list = new ArrayList<Integer>();
+        map = new HashMap<Integer, Integer>();
+    }
+
+    public void add(int number) {
+        // write your code here
+        if (map.containsKey(number)) {
+            map.put(number, map.get(number) + 1);
+        } else {
+            map.put(number, 1);
+            list.add(number);
+        }
+    }
+
+    /**
+     * @param value: An integer
+     * @return: Find if there exists any pair of numbers which sum is equal to the value.
+     */
+    public boolean find(int value) {
+        for (int i = 0; i < list.size(); i++) {
+            int num1 = list.get(i), num2 = value - num1;
+            if (((num1 == num2) && map.get(num1) > 1 ) || (num1 != num2) && map.containsKey(num2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
