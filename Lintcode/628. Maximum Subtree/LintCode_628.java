a89d76f155dd823046ed878a8a3e183ad57b641e
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-09-03 07:07:27 -0400

    Create LintCode_628.java

---
+++ Lintcode/628. Maximum Subtree/LintCode_628.java
@@ -0,0 +1,28 @@
+public class LintCode_628 {
+    /**
+     * @param root: the root of binary tree
+     * @return: the maximum weight node
+     */
+    private TreeNode result = null;
+    private int maximum_weight = Integer.MIN_VALUE;
+
+    public TreeNode findSubtree(TreeNode root) {
+        // write your code here
+        helper(root);
+        return result;
+    }
+
+    public int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left_weight = helper(root.left);
+        int right_weight = helper(root.right);
+
+        if (result == null || left_weight + right_weight + root.val > maximum_weight) {
+            maximum_weight = left_weight + right_weight + root.val;
+            result = root;
+        }
+        return left_weight + rightj_weight + root.val;
+    }
+}
