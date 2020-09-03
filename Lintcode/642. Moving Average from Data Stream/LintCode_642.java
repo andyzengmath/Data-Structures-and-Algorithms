81ed8fa16aeea26f9106d5aa132f882c304f526a
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-09-03 08:40:39 -0400

    Create LintCode_642.java

---
+++ Lintcode/642. Moving Average from Data Stream/LintCode_642.java
@@ -0,0 +1,28 @@
+public class LintCode_642 {
+
+    private Queue<Integer> queue;
+    private double sum = 0;
+    private int size;
+    /*
+    * @param size: An integer
+    */public MovingAverage(int size) {
+        // do intialization if necessary
+        queue = new LinkedList<Integer>();
+        this.size = size;
+
+    }
+
+    /*
+     * @param val: An integer
+     * @return:  
+     */
+    public double next(int val) {
+        // write your code here
+        sum += val;
+        if (queue.size() == size) {
+            sum = sum - queue.poll();
+        }
+        queue.offer(val);
+        return sum / queue.size();
+    }
+}
