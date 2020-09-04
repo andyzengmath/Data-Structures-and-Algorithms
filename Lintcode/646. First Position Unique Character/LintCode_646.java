6ea2b99b6d22dc79c0e96d8545075fd65630f79c
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-09-04 08:04:18 -0400

    Create LintCode_646.java

---
+++ Lintcode/646. First Position Unique Character/LintCode_646.java
@@ -0,0 +1,32 @@
+pulbic class LintCode_646 {
+    /**
+     * @param s: a string
+     * @return: it's index
+     */
+    public int firstUniqChar(String s) {
+        // hash
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Interger> charCount = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!charCount .containsKey(c)) {
+                charCount.put(c, 0);
+            }
+            charCount.put(c, (int)charCount.get(c) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ((int)charCount.get(c) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+
+
+    }
+}
