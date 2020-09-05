0832eee6fe0fa04554c147c163032a1e2d5129e0
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-09-05 11:12:10 -0400

    Create LintCode_686.java

---
+++ Lintcode/686. Remove Arbitrary Space/LintCode_686.java
@@ -0,0 +1,9 @@
+public class LintCode_686 {
+    /**
+     * @param s: the original string
+     * @return: the string without arbitrary spaces
+     */
+    public String removeExtra(String s) {
+        // write your code here
+    }
+}
