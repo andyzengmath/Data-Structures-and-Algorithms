b07d87e03004cb50aea53207243756e2dda4fe88
Andy <44346909+andyzengmath@users.noreply.github.com>
2020-09-05 11:20:29 -0400

    Update LintCode_686.java

--- Lintcode/686. Remove Arbitrary Space/LintCode_686.java
+++ Lintcode/686. Remove Arbitrary Space/LintCode_686.java
@@ -5,5 +5,24 @@ public class LintCode_686 {
      */
     public String removeExtra(String s) {
         // write your code here
+        String result = "";
+        for (int i = 0; i < s.length(); ) {
+            if (s.charAt(i) == ' ') {
+                boolean isntBegin = (i != 0);
+                while (i < s.length() && s.charAt(i) == ' ') {
+                    i++;
+                }
+                if (isntBegin && i < s.length()) {
+                    result += ' ';
+                }
+            }
+            while (i < s.length() && s.charAt(i) != ' ') {
+                result += s.charAt(i);
+                i++;
+            }
+        }
+        return result;
+
+    
     }
 }
