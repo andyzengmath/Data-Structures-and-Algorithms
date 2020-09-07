68536a5585f46bd9b37de382ca7a58e149b5eff4
Andy Zeng <acjzqy@gmail.com>
2020-09-07 11:14:13 -0400

    Create LintCode_769.java

---
+++ Lintcode/769. Spiral Array/LintCode_769.java
@@ -0,0 +1,9 @@
+public class LintCode_769 {
+    /**
+     * @param n: a Integer
+     * @return: a spiral array
+     */
+    public int[][] spiralArray(int n) {
+        // write your code here
+    }
+}
