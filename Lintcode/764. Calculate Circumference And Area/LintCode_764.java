b2f562af8e5cb930e20198d7e2b2e22a33d6c4de
Andy Zeng <acjzqy@gmail.com>
2020-09-07 09:07:45 -0400

    Create LintCode_764.java

---
+++ Lintcode/764. Calculate Circumference And Area/LintCode_764.java
@@ -0,0 +1,9 @@
+public class LintCode_764 {
+    /**
+     * @param r: a Integer represent radius
+     * @return: the circle's circumference nums[0] and area nums[1]
+     */
+    public double[] calculate(int r) {
+        // write your code here
+    }
+}
