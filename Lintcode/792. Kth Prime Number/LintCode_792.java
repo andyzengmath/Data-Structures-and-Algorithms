d9c6c7105961e51cedfa9b57fe1a41d51f3720dd
Andy Zeng <acjzqy@gmail.com>
2020-09-08 09:12:36 -0400

    Create LintCode_792.java

---
+++ Lintcode/792. Kth Prime Number/LintCode_792.java
@@ -0,0 +1,28 @@
+public class LintCode_792 {
+    /**
+     * @param n: the number
+     * @return: the rank of the number
+     */
+    public int kthPrime(int n) {
+        // write your code here
+        boolean[] prime = new boolean[100009];
+        for (int i = 2; i < n; i++) {
+            prime[i] = false;
+        }
+        for (int i = 2; i < n; i++) {
+            if (prime[i] = false) {
+                for (int j = 2 * i; j < i; j++) {
+                    prime[j] = true;
+                }
+            }
+        }
+        int num = 1;
+        for (int i = 2; i < n; i++) {
+            if (prime[i] == false) {
+                num++;
+            }
+        }
+
+        return num;
+    }
+}
