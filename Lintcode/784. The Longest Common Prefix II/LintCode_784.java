72f3c35d37a0e1b5304b4b7ec0283a0d9942d47f
Andy Zeng <acjzqy@gmail.com>
2020-09-08 08:09:49 -0400

    Create LintCode_784.java

---
+++ Lintcode/784. The Longest Common Prefix II/LintCode_784.java
@@ -0,0 +1,12 @@
+public class LintCode_784 {
+    /**
+     * @param dic: the n strings
+     * @param target: the target string
+     * @return: The ans
+     * The sum of the length of n strings is sum, 1 <= sum <= 50000. 
+     * The length of each string is greater than 0(i.e. there is no empty string).
+     */
+    public int the_longest_common_prefix(List<String> dic, String target) {
+        // write your code here
+    }
+}
