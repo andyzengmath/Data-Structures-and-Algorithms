edd0ba950280dafa24e618f23ad66d4270941004
Andy Zeng <acjzqy@gmail.com>
2020-09-09 07:18:09 -0400

    Update oneTraverse.java

--- Lintcode/807. Palindrome Number II/oneTraverse.java
+++ Lintcode/807. Palindrome Number II/oneTraverse.java
@@ -1,3 +1,19 @@
 public class oneTraverse(int n) {
-    
+    public boolean isPalindrome(int n) {
+        //一次循环，记录n 的reverse 返回n是否等于reverse
+        if (n < 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+        int compo = 0;
+        int copy = n;
+        while (copy != 0) {
+            compo *= 2;
+            compo += copy % 2 == 1 ? 1 : 0;
+            copy /= 2;
+        }
+        return compo == n;
+    }
 }
