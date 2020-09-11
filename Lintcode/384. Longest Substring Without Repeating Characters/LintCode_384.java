540e496af23c58656a12943c7f2f80450ca2a7ba
Andy Zeng <acjzqy@gmail.com>
2020-09-11 18:38:08 -0400

    Create LintCode_384.java

---
+++ Lintcode/384. Longest Substring Without Repeating Characters/LintCode_384.java
@@ -0,0 +1,30 @@
+public class LintCode_384 {
+    /** LeetCode 3
+     * @param s: a string
+     * @return: an integer
+     * Challenge: time complexity O(n)
+     */
+    public int lengthOfLongestSubstring(String s) {
+        // write your code here
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        HashSet<Character> window = new HashSet<>();
+        int left = 0, curLen = 0, maxLen = 0;
+        char[] sc = s.toCharArray();
+        for (char ch: sc) {
+            while (window.contains(ch)) {
+                window.remove(sc[left]);
+                left++;
+                curLen--;
+            }
+            window.add(ch);
+            curLen++;
+            maxLen = Math.max(maxLen, curLen);
+        }
+        return maxLen;
+
+
+
+    }
+}
