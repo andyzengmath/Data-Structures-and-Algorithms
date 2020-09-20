3731ff75a79067040609e94407f40dbf2f00b08b
Andy Zeng <acjzqy@gmail.com>
2020-09-20 09:19:22 -0400

    Create LintCode_428.java

---
+++ Lintcode/428. Pow(x, n)/LintCode_428.java
@@ -0,0 +1,20 @@
+public class LintCode_428 {
+    /**
+     * @param x: the base number
+     * @param n: the power number
+     * @return: the result
+     */
+    public double myPow(double x, int n) {
+        // write your code here
+        long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+
+    }
+    private double quickMul(double x, long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;
+    }
+}
