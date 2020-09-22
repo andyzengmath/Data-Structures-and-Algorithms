ebdcade9a43cb829aecd83b108542fbcfb8701f5
Andy Zeng <acjzqy@gmail.com>
2020-09-22 11:19:03 -0400

    Create LeetCode_58.java

---
+++ Lintcode/422. Length of Last Word/LeetCode_58.java
@@ -0,0 +1,18 @@
+class LeetCode_58 {
+    public int lengthOfLastWord(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int end = s.length() - 1;
+        while (end >= 0 && s.charAt(end) == ' ') {
+            end--;
+        }
+        
+        int start = end;
+        while (start >= 0 && s.charAt(start) != ' ') {
+            start--;
+        }
+        
+        return end - start;
+    }
+}
