caad102f4ba10083ae3e29ae267db9ffe6b64b4e
Andy Zeng <acjzqy@gmail.com>
2020-09-22 12:50:03 -0400

    Create LintCode_388.java
    hard!

---
+++ Lintcode/388. Permutation Sequence/LintCode_388.java
@@ -0,0 +1,35 @@
+public class LintCode_388 {
+    /** Leet-60
+     * hard
+     * @param n: n
+     * @param k: the k th permutation
+     * @return: return the k-th permutation
+     */
+    public String getPermutation(int n, int k) {
+        // write your code here
+        int[] factorial = new int[n];
+        factorial[0] = 1;
+        for (int i = 1; i < n; i++) {
+            factorial[i] = i * factorial[i - 1];
+        }
+        --k;
+        StringBuffer ans = new StringBuffer();
+        int[] valid = new int[n + 1];
+        Arrays.fill(valid, 1);
+
+        for (int i = 1; i <= n; i++) {
+            int order = k / factorial[n - i] + 1;
+            for (int j = 1; j <= n; j++) {
+                order -= valid[j];
+                if (order == 0) {
+                    ans.append(j);
+                    valid[j] = 0;
+                    break;
+                }
+            }
+            k %= factorial[n - i];
+        }
+
+        return ans.toString();
+    }
+}
