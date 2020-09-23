2bec753d36b0fd7e875c2a730ef05f4eb1d25cfb
Andy Zeng <acjzqy@gmail.com>
2020-09-23 11:19:14 -0400

    Create LeetCode_66.py

---
+++ Lintcode/407. Plus One/LeetCode_66.py
@@ -0,0 +1,12 @@
+class LeetCode_66:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] !=  9:
+                digits[i] += 1
+                return digits
+            else: 
+                digits[i] = 0
+                if digits[0] == 0:
+                    digits.insert(0, 1)
+                    return digits
+        
