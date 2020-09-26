35fffb12fe4f69c94519d6551af85f709ee068bb
Andy Zeng <acjzqy@gmail.com>
2020-09-26 12:18:42 -0400

    Create LintCode_510.java

---
+++ Lintcode/510. Maximal Rectangle/LintCode_510.java
@@ -0,0 +1,9 @@
+public class LintCode_510 {
+    /**
+     * @param matrix: a boolean 2D matrix
+     * @return: an integer
+     */
+    public int maximalRectangle(boolean[][] matrix) {
+        // write your code here
+    }
+}
