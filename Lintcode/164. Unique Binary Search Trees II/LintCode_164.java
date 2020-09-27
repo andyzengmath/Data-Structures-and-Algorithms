a16295e8957d96865330a5fbdbdb20144db3e012
Andy Zeng <acjzqy@gmail.com>
2020-09-27 12:41:42 -0400

    Create LintCode_164.java

---
+++ Lintcode/164. Unique Binary Search Trees II/LintCode_164.java
@@ -0,0 +1,46 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @paramn n: An integer
+     * @return: A list of root
+     */
+    public List<TreeNode> generateTrees(int n) {
+        // write your code here
+        
+        return generateTrees(1, n);
+    }
+
+    public List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new LinkedList<>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
+
+            for (TreeNode left: leftTrees) {
+                for (TreeNode right: rightTrees) {
+                    TreeNode curr = new TreeNode(i);
+                    curr.left = left;
+                    curr.right = right;
+                    allTrees.add(curr);
+                }
+            }
+        
+        }
+        return allTrees;
+    }
+}
