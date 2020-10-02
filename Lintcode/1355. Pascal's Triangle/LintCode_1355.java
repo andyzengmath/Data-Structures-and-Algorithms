68dfadf77bb88818ec391891bf0517106aa20487
Andy Zeng <acjzqy@gmail.com>
2020-10-02 13:23:06 -0400

    Create LintCode_1355.java

---
+++ Lintcode/1355. Pascal's Triangle/LintCode_1355.java
@@ -0,0 +1,27 @@
+public class LintCode_1355 {
+    /**
+     * @param numRows: num of rows
+     * @return: generate Pascal's triangle
+     */
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        if (numRows == 0) {
+            return triangle;
+        }
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
+        
+        for (int i = 1; i < numRows; i ++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> pre = triangle.get(i - 1);
+            row.add(1);
+            for (int j = 1; j < i; j++) {
+                row.add(pre.get(j) + pre.get(j - 1));
+            }
+            row.add(1);
+            triangle.add(row);
+        }
+        
+        return triangle;
+    }
+}
