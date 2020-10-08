b4c2bced3ab99204ebf1c56ef7ce7ed5c846428d
Andy Zeng <acjzqy@gmail.com>
2020-10-08 15:07:27 -0400

    Create LintCode_134.java

---
+++ Lintcode/134. LRU Cache/LintCode_134.java
@@ -0,0 +1,88 @@
+public class LRUCache {
+    /*
+    * @param capacity: An integer
+    */
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+
+        public DLinkedNode(int _key, int _value) {
+            key = _key;
+            value = _value;
+        }
+    }
+
+    private Map<Integer, DLinkedNode> cache = new HashMap<Integer, DLinkedNode>();
+    private int size;
+    private int capacity;
+    private DLinkedNode head, tail;
+    
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        head = new DLinkedNode(0, 0);
+        tail = new DLinkedNode(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    /*
+     * @param key: An integer
+     * @return: An integer
+     */
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) return -1;
+
+        moveToHead(node);
+        return node.value;
+    }
+
+    /*
+     * @param key: An integer
+     * @param value: An integer
+     * @return: nothing
+     */
+    public void set(int key, int value) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) {
+            DLinkedNode newNode = new DLinkedNode(key, value);
+            cache.put(key, newNode);
+            addToHead(newNode);
+            ++size;
+            if (size > capacity) {
+                DLinkedNode tail = removeTail();
+                cache.remove(tail.key);
+                --size;
+            }
+        } else {
+            node.value = value;
+            moveToHead(node);
+        }
+    }
+
+    private void addToHead(DLinkedNode node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(DLinkedNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void moveToHead(DLinkedNode node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private DLinkedNode removeTail() {
+        DLinkedNode res = tail.prev;
+        removeNode(res);
+        return res;
+    }
+}
