3f8b3603cadd77138f8421f9570e66cd48eb6ca6
Andy Zeng <acjzqy@gmail.com>
2020-10-09 14:20:31 -0400

    Create LintCode_160.java

---
+++ Lintcode/160. Find Minimum in Rotated Sorted Array II/LintCode_160.java
@@ -0,0 +1,22 @@
+public class LintCode_160 {
+    /**
+     * @param nums: a rotated sorted array
+     * @return: the minimum number in the array
+     */
+    public int findMin(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == -1) return -1;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+        return nums[left];
+    }
+}
