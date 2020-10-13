a9cf6b2580dd612758252b2b3016b5f0aa4967ef
Andy Zeng <acjzqy@gmail.com>
2020-10-12 23:48:20 -0400

    Create LintCode_760.java

---
+++ Lintcode/760. Binary Tree Right Side View/LintCode_760.java
@@ -0,0 +1,43 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class LintCode_760 {
+    /**
+     * @param root: the root of the given tree
+     * @return: the values of the nodes you can see ordered from top to bottom
+     */
+    public List<Integer> rightSideView(TreeNode root) {
+        // write your code here
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                if (i == size - 1) {
+                    result.add(node.val);
+                }
+            }
+        }
+
+        return result;
+    }
+}
