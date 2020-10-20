bd35eecde619c855f7e309684c4105d211a34b3f
Andy Zeng <acjzqy@gmail.com>
2020-10-20 11:54:00 -0400

    Create LeetCode_33.java

---
+++ Lintcode/62. Search in Rotated Sorted Array/LeetCode_33.java
@@ -0,0 +1,29 @@
+class Solution {
+    // review 10-20-2020
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+        if (nums.length == 1) return nums[0] == target ? 0 : -1;
+        int left = 0, right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) { // note has to include =, otherwise get stuck at e.g [3, 1]
+                if (nums[mid] > target && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
