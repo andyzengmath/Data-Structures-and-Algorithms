dc4723609592810881e05e5b4ceb6866d4f0c1e3
Andy Zeng <acjzqy@gmail.com>
2020-10-20 06:26:15 -0400

    Create LintCode_891.java

---
+++ Lintcode/891. Valid Palindrome II/LintCode_891.java
@@ -0,0 +1,27 @@
+public class LintCode_891 {
+    /**
+     * @param s: a string
+     * @return boolean: whether you can make s a palindrome by deleting at most one character
+     */
+    public boolean validPalindrome(String s) {
+        // Write your code here
+        int len = s.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - 1 - i)) {
+                return (isPalindrome(s, i, len - 2 - i) || isPalindrome(s, i + 1, len - 1 - i));
+            }
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
