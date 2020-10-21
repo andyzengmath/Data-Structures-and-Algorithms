b25aa0a93689228eb240ab0c07012ebcf3c4220d
Andy Zeng <acjzqy@gmail.com>
2020-10-21 17:30:49 -0400

    Create LintCode_1876.java

---
+++ Lintcode/1876. Alien Dictionary(easy)/LintCode_1876.java
@@ -0,0 +1,10 @@
+public class LintCode_1876 {
+    /**
+     * @param words: the array of string means the list of words
+     * @param order: a string indicate the order of letters
+     * @return: return true or false
+     */
+    public boolean isAlienSorted(String[] words, String order) {
+        // 
+    }
+}
