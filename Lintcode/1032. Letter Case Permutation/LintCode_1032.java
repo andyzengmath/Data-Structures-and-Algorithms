e2b4633beb6df40000df1bb2576bc86017e7b564
Andy Zeng <acjzqy@gmail.com>
2020-10-22 07:50:07 -0400

    Create LintCode_1032.java

---
+++ Lintcode/1032. Letter Case Permutation/LintCode_1032.java
@@ -0,0 +1,31 @@
+public class LintCode_1032 {
+    /**
+     * @param S: a string
+     * @return: return a list of strings
+     */
+    public List<String> letterCasePermutation(String S) {
+        // write your code here
+        StringBuilder sb = new StringBuilder();
+        List<String> res = new ArrayList<>();
+        char[] chArr= S.toCharArray();
+        dfs(chArr, 0, res);
+        return res;
+    }
+    
+    private void dfs(char[] chArr, int idx, List<String> res) {
+        if (idx == chArr.length) {
+            res.add(new String(chArr));
+            return ;
+        }
+        dfs(chArr, idx + 1, res);
+        if (chArr[idx] >= 'A') {
+            if (chArr[idx] < 'a') {
+                chArr[idx] = (char)(chArr[idx] + 32);
+            } else {
+                chArr[idx] = (char)(chArr[idx] - 32);
+            }
+            dfs(chArr, idx + 1, res);
+        }
+        
+    }
+}
