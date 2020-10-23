c66488322de3a216781778a668abdcb69be2f1b2
Andy Zeng <acjzqy@gmail.com>
2020-10-22 20:14:04 -0400

    Create LeetCode_102.java

---
+++ Lintcode/69. Binary Tree Level Order Traversal/LeetCode_102.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class LeetCode_102 {
+    // 10-22-20 review
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<>();
+        if (root == null) return levels;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root); // or add
+        
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size(); // note: don't pass queue.size() in loop
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            levels.add(level);
+        }
+        return levels;
+    }
+}
