896fc8b68a04cabdf86e213e25a417b3f69e2a8b
Andy Zeng <acjzqy@gmail.com>
2020-10-31 11:24:18 -0400

    Update LeetCode_102.java

--- Lintcode/69. Binary Tree Level Order Traversal/LeetCode_102.java
+++ Lintcode/69. Binary Tree Level Order Traversal/LeetCode_102.java
@@ -35,4 +35,25 @@ class LeetCode_102 {
         return levels;
     }
     
+
+    // recursive
+    List<List<Integer>> levels = new ArrayList<List<Integer>>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return levels;
+        helper(root, 0);
+        return levels;
+    }
+
+    private void helper(TreeNode node, int level) {
+        if (levels.size() == level) {
+            levels.add(new ArrayList<Integer>());
+        }
+        levels.get(level).add(node.val);
+        if (node.left != null) {
+            helper(node.left, level + 1);
+        }
+        if (node.right != null) {
+            helper(node.right, level + 1);
+        }
+    }
 }
