e014628c8d265e7b0a66e4e6da2ca1544fa9d29e
Andy Zeng <acjzqy@gmail.com>
2020-11-03 12:12:40 -0500

    Create LintCode_1351.java

---
+++ Lintcode/1351. Fraction to Recurring Decimal/LintCode_1351.java
@@ -0,0 +1,41 @@
+public class LintCode_1351 {
+    /**
+     * @param numerator: a integer
+     * @param denominator: a integer
+     * @return: return a string
+     */
+    public String fractionToDecimal(int numerator, int denominator) {
+        // write your code here
+        if (numerator == 0) {
+            return "0";
+        }
+        StringBuilder fraction = new StringBuilder();
+        
+        if (numerator < 0 ^ denominator < 0) {
+            fraction.append("-");
+        }
+        
+        long dividend = Math.abs(Long.valueOf(numerator));
+        long divisor = Math.abs(Long.valueOf(denominator));
+        fraction.append(String.valueOf(dividend / divisor));
+        long remainder = dividend % divisor;
+        if (remainder == 0) return fraction.toString();
+        
+        fraction.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        
+        while (remainder != 0) {
+            if (map.containsKey(remainder)) {
+                fraction.insert(map.get(remainder), "(");
+                fraction.append(")");
+                break;
+            }
+            map.put(remainder, fraction.length());
+            remainder *= 10;
+            fraction.append(String.valueOf(remainder / divisor));
+            remainder %= divisor;
+        }
+        return fraction.toString();
+        
+    }
+}
