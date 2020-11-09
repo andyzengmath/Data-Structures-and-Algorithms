94dee222a6810cdd17d844b047fa5e53825623cb
Andy Zeng <acjzqy@gmail.com>
2020-11-08 23:56:12 -0500

    add 1380

--- Lintcode/1380. Log Sorting/LintCode_1380.java
+++ Lintcode/1380. Log Sorting/LintCode_1380.java
@@ -1,35 +1,23 @@
 public class LintCode_1380 {
     /**
-     * @param nums: A list of integers
-     * @return: the median of numbers
+     * @param logs: the logs
+     * @return: the log after sorting
      */
-    private int count;
-    private PriorityQueue<Integer> maxheap, minheap;
-    
-    public int[] medianII(int[] nums) {
-        // write your code here
-        count = 0;
-        maxheap = new PriorityQueue<>((x, y) -> y - x);
-        minheap = new PriorityQueue<>();
-        int[] res = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            addNum(nums[i]);
-            res[i] = findMedian();
-        }
-        return res;
-        
-    }
-    
-    public void addNum(int num) {
-        count += 1;
-        maxheap.offer(num);
-        minheap.add(maxheap.poll());
-        if ((count & 1) !=  0) {
-            maxheap.add(minheap.poll());
-        }
-    }
-    
-    public int findMedian() {
-        return maxheap.peek();
+    public String[] logSort(String[] logs) {
+        // Write your code here
+        Arrays.sort(logs, (log1, log2) -> {
+            String[] split1 = log1.split(" ", 2);
+            String[] split2 = log2.split(" ", 2);
+            boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+            boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+            if (!isDigit1 && !isDigit2) {
+                int res = split1[1].compareTo(split2[1]);
+                if (res != 0) return res;
+                return split1[0].compareTo(split2[0]);
+            }
+            
+            return isDigit1 ? (isDigit2 ? 0 : 1) : - 1;
+        });
+        return logs;
     }
 }
