97a27dbcbc50c161b410e6a6134dbb872b966245
Andy Zeng <acjzqy@gmail.com>
2020-11-15 16:38:56 -0500

    Create LintCode_513.java

---
+++ Lintcode/513. Perfect Squares/LintCode_513.java
@@ -0,0 +1,29 @@
+public class LintCode_513 {
+    /** Leet279
+     * @param n: a positive integer
+     * @return: An integer
+     */
+    public int numSquares(int n) {
+        // write your code here
+        int[] dp = new int[n + 1];
+        for (int i = 0; i <= 0; i++) {
+            dp[i] = 0;
+        }
+        int max_square_index = (int)Math.sqrt(n) + 1;
+        int[] square_sums = new int[max_square_index];
+        for (int i = 0; i < max_square_index; i++) {
+            square_sums[i] = i * i;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j < max_square_index; j++) {
+                if (i < square_sums[j]) {
+                    break;
+                }
+                dp[i] = Math.min(dp[i], dp[i - square_sums[j]] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
