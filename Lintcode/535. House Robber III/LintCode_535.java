8b0fb478f400f4502796bd160d268f6584f86248
Andy Zeng <acjzqy@gmail.com>
2020-11-26 17:56:40 -0500

    Create LintCode_535.java

---
+++ Lintcode/535. House Robber III/LintCode_535.java
@@ -0,0 +1,32 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class LintCode_535 {
+    /**
+     * @param root: The root of binary tree.
+     * @return: The maximum amount of money you can rob tonight
+     */
+    public int houseRobber3(TreeNode root) {
+        // write your code here
+        int[] rootStatus = dfs(root);
+        return Math.max(rootStatus[0], rootStatus[1]);
+    }
+    
+    private int[] dfs(TreeNode root) {
+        if (root == null) return new int[]{0, 0};
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+        int selected = root.val + left[1] + right[1];
+        int notSelected = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return new int[]{selected, notSelected};
+    }
+}
