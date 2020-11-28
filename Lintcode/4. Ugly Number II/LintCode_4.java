8f5865fd87c61c06845eb87739c8960fe456e541
Andy Zeng <acjzqy@gmail.com>
2020-11-28 16:40:24 -0500

    Create LintCode_4.java

---
+++ Lintcode/4. Ugly Number II/LintCode_4.java
@@ -0,0 +1,27 @@
+public class LintCode_4 {
+    /**
+     * @param n: An integer
+     * @return: return a  integer as description.
+     */
+    public int nthUglyNumber(int n) {
+        // write your code here
+        int[] nums = new int[n];
+        nums[0] = 1;
+        int i2 = 0, i3 = 0, i5 = 0;
+        int temp = 1;
+        for (int i = 1; i < n; i++) {
+            temp = Math.min(Math.min(nums[i2] * 2, nums[i3] * 3), nums[i5] * 5);
+            nums[i] = temp;
+            if (temp == nums[i2] * 2) {
+                i2++;
+            }
+            if (temp == nums[i3] * 3) {
+                i3++;
+            }
+            if (temp == nums[i5] * 5) {
+                i5++;
+            }
+        }
+        return nums[n - 1];
+    }
+}
