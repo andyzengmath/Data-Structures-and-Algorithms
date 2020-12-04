5fd015dba893346ab67a693c9fd0f6cb21cf1a7f
Andy Zeng <acjzqy@gmail.com>
2020-12-03 21:47:57 -0500

    Create LintCode_664.java

---
+++ Lintcode/664. Counting Bits/LintCode_664.java
@@ -0,0 +1,14 @@
+public class LintCode_664 {
+    /**
+     * @param num: a non negative integer number
+     * @return: an array represent the number of 1's in their binary
+     */
+    public int[] countBits(int num) {
+        // write your code here
+        int[] ans = new int[num + 1];
+        for (int i = 1; i <= num; i++) {
+            ans[i] = ans[i & (i - 1)] + 1;
+        }
+        return ans;
+    }
+}
