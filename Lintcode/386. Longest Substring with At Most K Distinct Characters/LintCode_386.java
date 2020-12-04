1aaeca3d93ae7dc83c94e604c8db0430fd7bc0dc
Andy Zeng <acjzqy@gmail.com>
2020-12-04 17:17:57 -0500

    add 17

---
+++ Lintcode/386. Longest Substring with At Most K Distinct Characters/LintCode_386.java
@@ -0,0 +1,10 @@
+public class LintCode_386 {
+    /**
+     * @param s: A string
+     * @param k: An integer
+     * @return: An integer
+     */
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        // write your code here
+    }
+}
