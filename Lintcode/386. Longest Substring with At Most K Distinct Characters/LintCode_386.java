2924b47cd18886e7baa8ecb7c038cd9f40e47864
Andy Zeng <acjzqy@gmail.com>
2020-12-04 20:14:43 -0500

    Update LintCode_386.java

--- Lintcode/386. Longest Substring with At Most K Distinct Characters/LintCode_386.java
+++ Lintcode/386. Longest Substring with At Most K Distinct Characters/LintCode_386.java
@@ -6,5 +6,24 @@ public class LintCode_386 {
      */
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
         // write your code here
+        int n = s.length();
+        if (n * k == 0) {
+            return 0;
+        }
+
+        int left = 0, right = 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max_len = 1;
+        while (right < n) {
+            map.put(s.charAt(right), right++);
+            if (map.size() == k + 1) {
+                int idx = Collections.min(map.values());
+                map.remove(s.charAt(idx));
+                left = idx + 1;
+            }
+            max_len = Math.max(max_len, right - left);
+        }
+        return max_len;
+
     }
 }
