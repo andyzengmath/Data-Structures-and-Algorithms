85e8aedce25e0a60460691b77690a548f20bc8f1
Andy Zeng <acjzqy@gmail.com>
2020-12-09 06:56:13 -0500

    Create LintCode_1300.java

---
+++ Lintcode/1300. Bash Game/LintCode_1300.java
@@ -0,0 +1,49 @@
+public class LintCode_1300 {
+    /**
+     * @param n: an integer
+     * @return: whether you can win the game given the number of stones in the heap
+     */
+    public boolean canWinBash(int n) {
+        // Write your code here
+        return memoSearch(n, new HashMap<>());
+    }
+
+    private boolean momoSearch(int n, Map<Integer, Boolean> memo) {
+        if (n <= 3) return true;
+
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            if (!memoSearch(n - i), memo)) {
+                memo.put(n, true);
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+public boolean canWinBash(int n) {
+    // Write your code here
+    if (n <= 3) return true;
+    
+    boolean[] dp = new boolean[3];
+    for (int i = 0; i <= 2; i++) {
+        dp[i] = true;
+    }
+    for (int i = 3; i < n; i++) {
+        dp[i % 3] = !dp[(i - 1) % 3] || !dp[(i -2) % 3] || !dp[(i - 3) % 3];
+    }
+    return dp[(n - 1) % 3];
+}
+
+public boolean canWinBash(int n) {
+    // Write your code here
+    if (n % 4 == 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
