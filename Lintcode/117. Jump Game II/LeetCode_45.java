b79bad0748df7c1c5d776fc7b36aed6bebcca0bf
Andy Zeng <acjzqy@gmail.com>
2020-12-09 23:09:13 -0500

    Create LeetCode_45.java

---
+++ Lintcode/117. Jump Game II/LeetCode_45.java
@@ -0,0 +1,34 @@
+class LeetCode_45 {
+    // from back to front
+    public int jump(int[] nums) {
+        int position = nums.length - 1;
+        int steps = 0;
+        while (position > 0) {
+            for (int i = 0; i < position; i++) {
+                if (i + nums[i] >= position) {
+                    position = i;
+                    steps++;
+                    break;
+                }
+            }
+        }
+        return steps;
+    }
+}
+
+class Solution {
+    public int jump(int[] nums) {
+        int length = nums.length;
+        int end = 0;
+        int maxPosition = 0;
+        int steps = 0;
+        for (int i = 0; i < length - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if (i == end) {
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+    }
+}
