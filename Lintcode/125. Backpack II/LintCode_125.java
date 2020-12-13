e7fe9ceb57266382a64fbb8f3e61ef5621827996
Andy Zeng <acjzqy@gmail.com>
2020-12-13 15:37:57 -0500

    add 125

---
+++ Lintcode/125. Backpack II/LintCode_125.java
@@ -0,0 +1,26 @@
+public class Solution {
+    /**
+     * @param m: An integer m denotes the size of a backpack
+     * @param A: Given n items with size A[i]
+     * @param V: Given n items with value V[i]
+     * @return: The maximum value
+     */
+    public int backPackII(int m, int[] A, int[] V) {
+        // write your code here
+        if (A == null || V == null) {
+            return 0;
+        }
+        int n = A.length;
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (j >= A[i - 1]) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - A[i - 1]] + V[i - 1]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
