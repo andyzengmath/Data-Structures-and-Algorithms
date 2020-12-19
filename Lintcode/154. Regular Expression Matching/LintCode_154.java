4dbe11a68a0b16f7cd57309d79738937b777fd83
Andy Zeng <acjzqy@gmail.com>
2020-12-18 23:01:24 -0500

    Update LintCode_154.java

--- Lintcode/154. Regular Expression Matching/LintCode_154.java
+++ Lintcode/154. Regular Expression Matching/LintCode_154.java
@@ -5,10 +5,62 @@ public class LintCode_154 {
      * @return: A boolean
      */
     public boolean isMatch(String text, String pattern) {
-        // recursion
-        // too slow!
+        if (text == null || pattern == null) {
+            return false;
+        }
+
+        boolean[][] memo = new boolean[text.length()][pattern.length()];
+        boolean[][] visited = new boolean[text.length()][pattern.length()];
+
+        return isMatchHelper(text, 0, pattern, 0, memo, visited);
+    }
+
+    private boolean isMatchHelper(String s, int sIndex, String p, int pIndex, boolean[][] memo, boolean[][] visited) {
+        if (pIndex == p.length()) {
+            return sIndex == s.length();
+        }
+        if (sIndex == s.length()) {
+            return isEmpty(p, pIndex);
+        }
+
+        if (visited[sIndex][pIndex]) {
+            return memo[sIndex][pIndex];
+        }
+        char sChar = s.charAt(sIndex);
+        char pChar = p.charAt(pIndex);
+        boolean match;
+
+        if (pIndex + 1 < p.length() && p.charAt(pIndex + 1) == '*') {
+            match = isMatchHelper(s, sIndex, p, pIndex + 2, memo, visited) || 
+                    charMatch(sChar, pChar) && isMatchHelper(s, sIndex + 1, p, pIndex, memo, visited);  
+        } else {
+            match = charMatch(sChar, pChar) && isMatchHelper(s, sIndex + 1, p, pIndex + 1, memo, visited);
+        }
+        visited[sIndex][pIndex] = true;
+        memo[sIndex][pIndex] = match;
+        return match;
+    }
+
+    private boolean charMatch(char sChar, char pChar) {
+        return sChar == pChar || pChar == '.';
+    }
+
+    private boolean isEmpty (String p, int pIndex) {
+        for (int i = pIndex; i < p.length(); i += 2) {
+            if (i + 1 >= p.length() || p.charAt(i + 1) != '*') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+
+class Solution {
+    public boolean isMatch(String text, String pattern) {
         if (pattern.isEmpty()) return text.isEmpty();
         boolean firstMatch = (!text.isEmpty() && (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+        
         if (pattern.length() >= 2 && pattern.charAt(1) == '*') {
             return (isMatch(text, pattern.substring(2)) || (firstMatch && isMatch(text.substring(1), pattern)));
         } else {
