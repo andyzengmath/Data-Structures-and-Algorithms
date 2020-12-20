63d15d4e50abbc8fe8a27de19acc30683d1ca021
Andy Zeng <acjzqy@gmail.com>
2020-12-20 12:05:55 -0500

    Create LeetCode_329.java

---
+++ Lintcode/398. Longest Continuous Increasing Subsequence II/LeetCode_329.java
@@ -0,0 +1,46 @@
+class LeetCode_329 {
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) return 0;
+        if (matrix[0] == null || matrix[0].length == 0) return 0;
+
+        int n = matrix.length, m = matrix[0].length;
+        int[] dx = {0, 0 ,1, -1};
+        int[] dy = {1, -1, 0, 0};
+
+        List<List<Integer>> points = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                points.add(Arrays.asList(i, j, matrix[i][j]));
+            }
+        }
+        points.sort((p1, p2) -> Integer.compare(p1.get(2), p2.get(2)));
+
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < points.size(); i++) {
+            int x = points.get(i).get(0);
+            int y = points.get(i).get(1);
+
+            dp[x][y] = 1;
+
+            for (int j = 0; j < 4; j++) {
+                int prevX = x - dx[j];
+                int prevY = y - dy[j];
+
+                if (prevX < 0 || prevX >= n || prevY < 0 || prevY >= m) {
+                    continue;
+                }
+                if (matrix[prevX][prevY] >= matrix[x][y]) {
+                    continue;
+                }
+                dp[x][y] = Math.max(dp[x][y], dp[prevX][prevY] + 1);
+            }
+        }
+        int longest = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                longest = Math.max(longest, dp[i][j]);
+            }
+        }
+        return longest;
+    }
+}
