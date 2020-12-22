3b0ad95e253383d23b71d4d5df99a737ff48ff68
Andy Zeng <acjzqy@gmail.com>
2020-12-21 20:17:54 -0500

    Create LintCode_610.java

---
+++ Lintcode/610. Two Sum - Difference equals to target/LintCode_610.java
@@ -0,0 +1,24 @@
+class LintCode_610 {
+    // locked 
+    public int[] twoSum7(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return new int[]{-1, -1};
+        }
+
+        target = Math.abs(target);
+        int j = 1;
+        for (int i = 0; i < nums.length; i++) {
+            j = Math.max(j, i + 1);
+            while (j < nums.length && nums[j] - nums[i] < target) {
+                j++;
+            }
+            if (j >= nums.length) {
+                break;
+            }
+            if (nums[j] - nums[i] == target) {
+                return new int[]{nums[i], nums[j]};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
