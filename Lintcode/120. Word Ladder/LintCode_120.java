9276a29c80c023344a0810898667b0b890cd56b5
Andy Zeng <acjzqy@gmail.com>
2020-12-27 16:19:22 -0500

    Update LintCode_120.java

--- Lintcode/120. Word Ladder/LintCode_120.java
+++ Lintcode/120. Word Ladder/LintCode_120.java
@@ -64,4 +64,85 @@ public class LintCode_120 {
         }
         return nextWords;
     }
+
+
+    // JiuZhang double BFS version
+    public int ladderLength(String start, String end, Set<String> wordSet) {
+        if (start.equals(end)) {
+            return 1;
+        }
+        HashMap<String, Set<String>> graph;
+        Queue<String> forwardQueue = new LinkedList<String>();
+        Queue<String> backwardQueue = new LinkedList<String>();
+        Set<String> forwardSet = new HashSet<String>();
+        Set<String> backwardSet = new HashSet<String>();
+        
+        wordSet.add(start);
+        wordSet.add(end);
+        graph = constructGraph(wordSet);
+        forwardQueue.offer(start);
+        backwardQueue.offer(end);
+        forwardSet.add(start);
+        backwardSet.add(end);
+
+        int distance = 1;
+        while (!forwardQueue.isEmpty() && !backwardQueue.isEmpty()) {
+            distance++;
+            if (extendQueue(graph, forwardQueue, forwardSet, backwardSet)) {
+                return distance;
+            }
+            distance++;
+            if (extendQueue(graph, backwardQueue, backwardSet, forwardSet)) {
+                return distance;
+            }
+        }
+        return -1;
+    }
+
+    boolean extendQueue(HashMap<String, Set<String>> graph, Queue<String> queue, Set<String> visited, Set<String> oppositeVisited) {
+        int queueLength = queue.size();
+        for (int i = 0; i < queueLength; i++) {
+            String word = queue.poll();
+            Set<String> nextWordSet = graph.get(word);
+            for (String nextWord: nextWordSet) {
+                if (visited.contains(nextWord)) {
+                    continue;
+                }
+                if (oppositeVisited.contains(nextWord)) {
+                    return true;
+                }
+                queue.offer(nextWord);
+                visited.add(nextWord);
+            }
+        }
+        return false;
+    }
+
+    HashMap<String, Set<String>> constructGraph(Set<String> wordSet) {
+        HashMap<String, Set<String>> graph = new HashMap<String, Set<String>>();
+        for (String word: wordSet) {
+            graph.put(word, getNextWords(word, wordSet));
+        }
+        return graph;
+    }
+
+    Set<String> getNextWords(String word, Set<String> wordSet) {
+        Set<String> nextWordSet = new HashSet<String>();
+        int wordLength = word.length();
+        for (int i = 0; i < wordLength; i++) {
+            String prefix = word.substring(0, i);
+            String suffix = word.substring(i + 1);
+            char[] chars = ("abcdefghijklmnopqrstuvwxyz").toCharArray();
+            for (int j = 0; j < 26; j++) {
+                if (word.charAt(i) == chars[j]) {
+                    continue;
+                }
+                String nextWord = prefix + chars[j] + suffix;
+                if (wordSet.contains(nextWord)) {
+                    nextWordSet.add(nextWord);
+                }
+            }
+        }
+        return nextWordSet;
+    }
 }
