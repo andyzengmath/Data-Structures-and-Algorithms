ace1bb9dbd3144876818558577a11de8bb5fc8d1
Andy Zeng <acjzqy@gmail.com>
2021-01-08 10:22:23 -0500

    Update LintCode_512.java
    add rolling array

--- Lintcode/512. Decode Ways/LintCode_512.java
+++ Lintcode/512. Decode Ways/LintCode_512.java
@@ -31,4 +31,30 @@ public class LintCode_512 {
         return dp[0];
         
     }
+    // 滚动数组
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[3];
+        dp[0] = 1;
+        dp[1] = decodeOK(s.substring(0, 1));
+        for (int i = 2; i <= n; i++) {
+            dp[i % 3] = dp[(i - 1) % 3] * decodeOK(s.substring(i - 1, i)) + dp[(i - 2) % 3] * decodeOK(s.substring(i - 2, i));
+        }
+        return dp[n % 3];
+    }
+
+    private int decodeOK(String substr) {
+        int code = Integer.parseInt(substr);
+        int len = substr.length();
+        if (len == 1 && code >= 1 && code <= 9) {
+            return 1;
+        }
+        if (len == 2 && code >= 10 && code <= 26) {
+            return 1;
+        }
+        return 0;
+    }
 }
