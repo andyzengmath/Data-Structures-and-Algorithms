c5fa10254ed867c803225b291f6e17eee51557f9
Andy Zeng <acjzqy@gmail.com>
2021-01-10 17:16:41 -0500

    Create LintCode_667.java

---
+++ Lintcode/667. Longest Palindromic Subsequence/LintCode_667.java
@@ -0,0 +1,27 @@
+public class LintCode_667 {
+    /**
+     * @param s: the maximum length of s is 1000
+     * @return: the longest palindromic subsequence's length
+     */
+    public int longestPalindromeSubseq(String s) {
+        // write your code here
+        int n = s.length();
+        char[] ch = s.toCharArray();
+        if (n <= 1) return n;
+
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (ch[i] == ch[j]) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
