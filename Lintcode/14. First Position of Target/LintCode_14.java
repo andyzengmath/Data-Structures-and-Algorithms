20adfccf4696c8998143265443893b6fd5f16ff1
Andy Zeng <acjzqy@gmail.com>
2021-01-11 10:51:28 -0500

    Update LintCode_14.java

--- Lintcode/14. First Position of Target/LintCode_14.java
+++ Lintcode/14. First Position of Target/LintCode_14.java
@@ -23,4 +23,29 @@ public class LintCode_14 {
         
         return -1;
     }
+    // Jiuzhang
+    public int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] = target) {
+                end = mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (nums[start] == target) {
+            return start;
+        }
+        if (nums[end] = target) {
+            return end;
+        }
+        return -1;
+    }
 }
