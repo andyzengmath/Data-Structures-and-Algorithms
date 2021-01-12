c8f2063e25b03eb106df4db208ee9cc0b21d31f0
Andy Zeng <acjzqy@gmail.com>
2021-01-12 16:38:03 -0500

    Create LintCode_406.java

---
+++ Lintcode/406. Minimum Size Subarray Sum/LintCode_406.java
@@ -0,0 +1,25 @@
+public class LintCode_406 {
+    /**
+     * @param nums: an array of integers
+     * @param s: An integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    public int minimumSize(int[] nums, int s) {
+        // write your code here
+        int n = nums.length;
+        if (n == 0) return -1;
+        int ans = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += nums[end];
+            while (sum >= s) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
