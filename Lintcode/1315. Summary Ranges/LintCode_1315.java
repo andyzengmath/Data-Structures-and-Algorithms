73e56af6e7ea68749be15b28d42f881054d1c2c2
Andy Zeng <acjzqy@gmail.com>
2021-01-13 14:46:09 -0500

    add summary ranges

---
+++ Lintcode/1315. Summary Ranges/LintCode_1315.java
@@ -0,0 +1,26 @@
+public class LintCode_1315 {
+    /**
+     * @param nums:  a sorted integer array without duplicates
+     * @return: the summary of its ranges
+     */
+    public List<String> summaryRanges(int[] nums) {
+        // Write your code here
+        List<String> list = new ArrayList();
+        if (nums.length == 1) {
+            list.add(nums[0] + "");
+            return list;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int a = nums[i];
+            while (i + 1 < nums.length && (nums[i + 1] - nums[i]) == 1) {
+                i++;
+            }
+            if (a != nums[i]) {
+                list.add(a + "->" + nums[i]);
+            } else {
+                list.add(a + "");
+            }
+        }
+        return list;
+    }
+}
