a6b77ade5ea06ae5752f69102791fcb46aa536d3
Andy Zeng <acjzqy@gmail.com>
2021-01-15 15:02:03 -0500

    Create LintCode_1307.java

---
+++ Lintcode/1307. Verify Preorder Sequence in Binary Search Tree/LintCode_1307.java
@@ -0,0 +1,19 @@
+public class LintCode_1307 {
+    /**
+     * @param preorder: List[int]
+     * @return: return a boolean
+     */
+    public boolean verifyPreorder(int[] preorder) {
+        // write your code here
+        int low = Integer.MIN_VALUE;
+        Stack<Integer> path = new Stack();
+        for (int p: preorder) {
+            if (p < low) return false;
+            while (!path.empty() && p > path.peek()) {
+                low = path.pop();
+            }
+            path.push(p);
+        }
+        return true;
+    }
+}
